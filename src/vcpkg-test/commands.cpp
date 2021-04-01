0ef47cb86226020c17bb59e19275430a5abc42ca
Nicole Mazzuca <mazzucan@outlook.com>
2021-04-01 11:49:32 -0700

    fix test

--- src/vcpkg-test/commands.cpp
+++ src/vcpkg-test/commands.cpp
@@ -78,7 +78,8 @@ TEST_CASE ("get_available_commands_type_a works", "[commands]")
         "build-external",
         "export",
         "depend-info",
-        "x-check-support"
+        "x-check-support",
+        "x-print-default-triplets",
         });
 }
 // clang-format on
