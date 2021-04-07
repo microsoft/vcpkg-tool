0c847d08baeedb03588a3fed2b56b288c5fa8967
k1ee <cnschwarzer@qq.com>
2021-04-08 02:14:12 +0800

    Reformat

--- src/vcpkg/base/system.proxy.cpp
+++ src/vcpkg/base/system.proxy.cpp
@@ -7,17 +7,14 @@ std::optional<std::string> vcpkg::System::get_windows_proxy_server()
 
     auto ret =
         RegOpenKey(HKEY_CURRENT_USER, R"(Software\Microsoft\Windows\CurrentVersion\Internet Settings)", &proxy_reg);
-    if (ret)
-        return nullptr;
+    if (ret) return nullptr;
 
     ret = RegGetValue(proxy_reg, nullptr, "ProxyServer", RRF_RT_REG_SZ, nullptr, nullptr, &buf_len);
-    if (ret)
-        return nullptr;
+    if (ret) return nullptr;
 
     std::string srv(buf_len, '\0');
     ret = RegGetValue(proxy_reg, nullptr, "ProxyServer", RRF_RT_REG_SZ, nullptr, srv.data(), &buf_len);
-    if (ret)
-        return nullptr;
+    if (ret) return nullptr;
 
     return srv;
 #else
