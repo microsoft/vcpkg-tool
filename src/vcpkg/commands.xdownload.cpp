52cd842f422dfae29d1f226681e0478da8be00db
Billy Robert O'Neal III <bion@microsoft.com>
2021-06-17 16:04:06 -0700

    Change x-download into a basic command so that it doesn't need to find a vcpkg-root etc.

--- src/vcpkg/commands.xdownload.cpp
+++ src/vcpkg/commands.xdownload.cpp
@@ -3,10 +3,11 @@
 #include <vcpkg/base/parse.h>
 #include <vcpkg/base/system.debug.h>
 #include <vcpkg/base/system.print.h>
+#include <vcpkg/base/util.h>
 
+#include <vcpkg/binarycaching.h>
 #include <vcpkg/commands.xdownload.h>
 #include <vcpkg/vcpkgcmdarguments.h>
-#include <vcpkg/vcpkgpaths.h>
 
 namespace vcpkg::Commands::X_Download
 {
@@ -38,11 +39,11 @@ namespace vcpkg::Commands::X_Download
     }
     static bool is_lower_sha512(StringView sha) { return sha.size() == 128 && is_lower_hex(sha); }
 
-    void perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths)
+    void perform_and_exit(const VcpkgCmdArguments& args, Files::Filesystem& fs)
     {
         auto parsed = args.parse_arguments(COMMAND_STRUCTURE);
-        fs::path file = Files::combine(paths.original_cwd, fs::u8path(args.command_arguments[0]));
-        file.make_preferred();
+        auto download_manager = create_download_manager(args.asset_sources_template).value_or_exit(VCPKG_LINE_INFO);
+        fs::path file = fs.absolute(VCPKG_LINE_INFO, fs::u8path(args.command_arguments[0]));
 
         std::string sha = Strings::ascii_to_lowercase(std::string(args.command_arguments[1]));
         if (!is_lower_sha512(sha))
@@ -50,8 +51,6 @@ namespace vcpkg::Commands::X_Download
             Checks::exit_with_message(
                 VCPKG_LINE_INFO, "Error: SHA512's must be 128 hex characters: '%s'", args.command_arguments[1]);
         }
-
-        auto& fs = paths.get_filesystem();
 
         // Is this a store command?
         if (Util::Sets::contains(parsed.switches, OPTION_STORE))
@@ -65,7 +64,7 @@ namespace vcpkg::Commands::X_Download
             auto hash =
                 Strings::ascii_to_lowercase(Hash::get_file_hash(VCPKG_LINE_INFO, fs, file, Hash::Algorithm::Sha512));
             if (hash != sha) Checks::exit_with_message(VCPKG_LINE_INFO, "Error: file to store does not match hash");
-            paths.get_download_manager().put_file_to_mirror(fs, file, sha).value_or_exit(VCPKG_LINE_INFO);
+            download_manager.put_file_to_mirror(fs, file, sha).value_or_exit(VCPKG_LINE_INFO);
             Checks::exit_success(VCPKG_LINE_INFO);
         }
         else
@@ -81,18 +80,18 @@ namespace vcpkg::Commands::X_Download
             auto it_urls = parsed.multisettings.find(OPTION_URL);
             if (it_urls == parsed.multisettings.end())
             {
-                paths.get_download_manager().download_file(fs, View<std::string>{}, headers, file, sha);
+                download_manager.download_file(fs, View<std::string>{}, headers, file, sha);
             }
             else
             {
-                paths.get_download_manager().download_file(fs, it_urls->second, headers, file, sha);
+                download_manager.download_file(fs, it_urls->second, headers, file, sha);
             }
             Checks::exit_success(VCPKG_LINE_INFO);
         }
     }
 
-    void XDownloadCommand::perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths) const
+    void XDownloadCommand::perform_and_exit(const VcpkgCmdArguments& args, Files::Filesystem& fs) const
     {
-        X_Download::perform_and_exit(args, paths);
+        X_Download::perform_and_exit(args, fs);
     }
 }
