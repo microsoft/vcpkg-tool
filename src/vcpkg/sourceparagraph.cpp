a668b96082742eb624c827f6444714bb6eb80f49
Billy Robert O'Neal III <bion@microsoft.com>
2021-07-30 14:15:04 -0700

    Remove "return std::move(local);"s.

--- src/vcpkg/sourceparagraph.cpp
+++ src/vcpkg/sourceparagraph.cpp
@@ -395,8 +395,9 @@ namespace vcpkg
 
         if (auto maybe_error = canonicalize(*control_file))
         {
-            return std::move(maybe_error);
+            return maybe_error;
         }
+
         return control_file;
     }
 
@@ -630,7 +631,7 @@ namespace vcpkg
                 type_name(), obj, DESCRIPTION, feature->description, Json::ParagraphDeserializer::instance);
             r.optional_object_field(obj, DEPENDENCIES, feature->dependencies, DependencyArrayDeserializer::instance);
 
-            return std::move(feature);
+            return feature;
         }
         static FeatureDeserializer instance;
     };
@@ -709,7 +710,7 @@ namespace vcpkg
                 }
             }
 
-            return std::move(res);
+            return res;
         }
 
         static FeaturesFieldDeserializer instance;
@@ -970,7 +971,8 @@ namespace vcpkg
             {
                 Checks::exit_with_message(VCPKG_LINE_INFO, maybe_error->error);
             }
-            return std::move(control_file);
+
+            return control_file;
         }
 
         static ManifestDeserializer instance;
@@ -1013,7 +1015,7 @@ namespace vcpkg
             auto err = std::make_unique<ParseControlErrorInfo>();
             err->name = origin;
             err->other_errors = std::move(reader.errors());
-            return std::move(err);
+            return err;
         }
         else if (auto p = res.get())
         {
