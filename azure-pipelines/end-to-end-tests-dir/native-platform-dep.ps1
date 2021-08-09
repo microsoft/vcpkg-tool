48e6fb185211eb9324504db83b16b8d9b57bd16e
nicole mazzuca <mazzucan@outlook.com>
2021-08-09 13:58:57 -0700

    fix bugs in end-to-end tests

--- azure-pipelines/end-to-end-tests-dir/native-platform-dep.ps1
+++ azure-pipelines/end-to-end-tests-dir/native-platform-dep.ps1
@@ -1,14 +1,11 @@
 . "$PSScriptRoot/../end-to-end-tests-prelude.ps1"
 
 if ($IsLinux) {
-    $triplet = 'x64-linux-e2e'
-    $host_triplet = 'x64-linux'
-} else if ($IsMacOS) {
-    $triplet = 'x64-osx-e2e'
-    $host_triplet = 'x64-osx'
-} else if ($IsWindows) {
-    $triplet = 'x64-windows-e2e'
-    $host_triplet = 'x64-windows'
+    $host_triplet = 'x64-linux-e2e'
+} elseif ($IsMacOS) {
+    $host_triplet = 'x64-osx-e2e'
+} elseif ($IsWindows) {
+    $host_triplet = 'x64-windows-e2e'
 } else {
     Write-Warning "Unknown platform."
     return
@@ -17,27 +14,12 @@ if ($IsLinux) {
 
 Write-Trace "test native qualified dependencies"
 
-$Overlays = "--overlay-ports=$PSScriptRoot/../e2e_ports/overlays", "--overlay-triplets=$PSScriptRoot/../e2e_ports/triplets"
-
-Run-Vcpkg x-set-installed
+Run-Vcpkg install @CommonArgs --host-triplet $Triplet vcpkg-native-dependency:$Triplet
 Throw-IfFailed
-Run-Vcpkg install @Overlays --triplet $Triplet --host-triplet $Triplet vcpkg-native-dependency
-Throw-IfFailed
-$ports = Run-Vcpkg list | % { (-split $_)[0] } | % { ($_ -split ':')[0] }
+Require-FileExists "$installRoot/$Triplet/share/vcpkg-empty-port"
+Refresh-TestRoot
 
-if ('vcpkg-empty-port' -notin $ports) {
-    throw "native platform dependency incorrect - vcpkg-empty-port not installed"
-}
-
-Run-Vcpkg x-set-installed
+Run-Vcpkg install @CommonArgs --host-triplet $host_triplet vcpkg-native-dependency:$Triplet
 Throw-IfFailed
-Run-Vcpkg install @Overlays --triplet $Triplet --host-triplet $Triplet vcpkg-native-dependency
-Throw-IfFailed
-$ports = Run-Vcpkg list | % { (-split $_)[0] } | % { ($_ -split ':')[0] }
-
-if ('vcpkg-empty-port' -in $ports) {
-    throw "native platform dependency incorrect - vcpkg-empty-port installed"
-}
-
-Run-Vcpkg x-set-installed
-Throw-IfFailed
+Require-FileNotExists "$installRoot/$Triplet/share/vcpkg-empty-port"
+Refresh-TestRoot
