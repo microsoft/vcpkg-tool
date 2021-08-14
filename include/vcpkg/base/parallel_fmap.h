a6147dc7e2ea42e95c3e1e45a58bd44133413742
Robert Schumacher <roschuma@microsoft.com>
2021-08-14 23:37:07 +0000

    Switch parallel fmap to queue instead of chunked

--- include/vcpkg/base/parallel_fmap.h
+++ include/vcpkg/base/parallel_fmap.h
@@ -17,32 +17,25 @@ namespace vcpkg::Util
             return {f(xs[0])};
         }
         std::vector<Out> res(xs.size());
+        std::atomic<size_t> work_item{0};
 
         const auto num_threads =
             static_cast<size_t>(std::max(1, std::min(get_concurrency(), static_cast<int>(xs.size()))));
 
-        auto work = [&xs, &res, &f, num_threads](const int first, const int last) {
-            for (int n = first; n < last; ++n)
+        auto work = [&xs, &res, &f, &work_item]() {
+            for (size_t item = work_item.fetch_add(1); item < xs.size(); item = work_item.fetch_add(1))
             {
-                res[n] = f(xs[n]);
+                res[item] = f(xs[item]);
             }
         };
 
         std::vector<std::thread> threads;
-        const auto chunksize = xs.size() / num_threads;
-        const auto bonus = xs.size() % num_threads;
-
-        size_t first = 0;
         for (size_t x = 0; x < num_threads - 1; ++x)
         {
-            const auto last = first + chunksize + (bonus > x ? 1 : 0);
-            Checks::check_exit(VCPKG_LINE_INFO, last < xs.size());
-            threads.emplace_back(work, first, last);
-            first = last;
+            threads.emplace_back(work);
+            if (work_item >= xs.size()) break;
         }
-        const auto last = first + chunksize;
-        Checks::check_exit(VCPKG_LINE_INFO, last == xs.size());
-        work(first, last);
+        work();
         for (auto&& th : threads)
         {
             th.join();
