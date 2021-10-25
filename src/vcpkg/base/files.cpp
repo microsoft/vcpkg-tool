01d64969aec1244cddbc4d21843fc740fe2c89d8
Billy Robert O'Neal III <bion@microsoft.com>
2021-10-25 13:21:19 -0700

    Fix copy_symlink on old libstdc++.
    `copy_symlink` had assumptions that `std::string` implements the small string optimization, and thus we would never call `readlink` with a buffer length of 0. Unfortunately, the old libstdc++ we want to use for old Linux uses the COW form where the initial capacity is 0, which caused `readlink` to fail with `EINVAL`.
    
    This change makes us always start with `PATH_MAX`.

--- src/vcpkg/base/files.cpp
+++ src/vcpkg/base/files.cpp
@@ -2748,7 +2748,7 @@ namespace vcpkg
             stdfs::copy_symlink(to_stdfs_path(source), to_stdfs_path(destination), ec);
 #else  // ^^^ _WIN32 // !_WIN32 vvv
             std::string buffer;
-            buffer.resize(buffer.capacity());
+            buffer.resize(PATH_MAX);
             for (;;)
             {
                 ssize_t result = ::readlink(source.c_str(), &buffer[0], buffer.size());
