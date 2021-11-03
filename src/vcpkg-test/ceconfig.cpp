cc58ccf80b2395e13155b38d90d8e44dd990c157
Victor Romero <viromer@microsoft.com>
2021-11-02 23:40:08 -0700

    Add parsing for ce fields in vcpkg-configuration.json

---
+++ src/vcpkg-test/ceconfig.cpp
@@ -0,0 +1,114 @@
+#include <catch2/catch.hpp>
+
+#include <vcpkg/base/json.h>
+#include <vcpkg/base/stringview.h>
+
+#include <vcpkg/configuration.h>
+
+#include <vcpkg-test/util.h>
+
+using namespace vcpkg;
+
+static Json::Object parse_json_object(StringView sv)
+{
+    auto json = Json::parse(sv);
+    // we're not testing json parsing here, so just fail on errors
+    if (auto r = json.get())
+    {
+        return std::move(r->first.object());
+    }
+    else
+    {
+        Checks::exit_with_message(VCPKG_LINE_INFO, json.error()->format());
+    }
+}
+
+static Configuration test_parse_configuration(StringView text, bool expect_fail = false)
+{
+    auto object = parse_json_object(text);
+
+    Json::Reader reader;
+    auto deserializer = make_configuration_deserializer("test");
+    auto parsed_config_opt = reader.visit(object, *deserializer);
+    CHECK((!expect_fail && reader.errors().empty()));
+
+    return std::move(parsed_config_opt).value_or_exit(VCPKG_LINE_INFO);
+}
+
+TEST_CASE ("ce config parse", "[configuration]")
+{
+    std::string ce_config_section = R"json(
+    "error": "this is an error",
+    "warning": "this is a warning",
+    "message": "this is a message",
+    "apply": {
+        "key": "value",
+        "complex-key": { "a": "apple", "b": "banana" }
+    },
+    "settings": {
+        "VCPKG_ROOT": "C:/Users/viromer/work/vcpkg",
+        "VCPKG_TARGET_TRIPLET": "arm-windows"
+    },
+    "requires": {
+        "tools/kitware/cmake": ">=3.21.0"
+    },
+    "see-also": {
+        "tools/ninja-build/ninja": "1.10.2"
+    },
+    "demands": {
+        "windows and target:arm": {
+            "requires": {
+                "compilers/microsoft/msvc/arm": "~17.0.0"
+            }
+        },
+        "linux and target:arm": {
+            "requires": {
+                "compilers/gnu/clang": "9.11.0"
+            },
+            "see-also": {}
+        },
+        "messages": {
+            "error": "this is a nested error",
+            "warning": "this is a nested warning",
+            "message": "this is a nested message"
+        }
+    }
+)json";
+
+    std::string raw_config = Strings::concat(R"json({
+    "default-registry": {
+        "kind": "builtin",
+        "baseline": "843e0ba0d8f9c9c572e45564263eedfc7745e74f"
+    },
+    "registries": [
+        {
+            "kind": "git",
+            "baseline": "dacf4de488094a384ca2c202b923ccc097956e0c",
+            "repository": "https://github.com/northwindtraders/vcpkg-registry",
+            "packages": [ "beicode", "beison" ]
+        }
+    ],
+)json",
+                                             ce_config_section,
+                                             "}");
+
+    auto config = test_parse_configuration(raw_config);
+    auto config_obj = serialize_configuration(config);
+    auto raw_config_obj = parse_json_object(raw_config);
+    const auto& parsed_ce_config = config.ce_configuration;
+
+    CHECK(!parsed_ce_config.is_empty());
+    REQUIRE(Json::stringify(config_obj, Json::JsonStyle::with_spaces(4)) ==
+            Json::stringify(raw_config_obj, Json::JsonStyle::with_spaces(4)));
+
+    auto check_string_value = [](const Json::Object& obj, StringView key, StringView expected) {
+        CHECK(obj.contains(key));
+        auto value = *obj.get(key);
+        CHECK(value.is_string());
+        REQUIRE(value.string() == expected);
+    };
+
+    check_string_value(parsed_ce_config, "error", "this is an error");
+    check_string_value(parsed_ce_config, "warning", "this is a warning");
+    check_string_value(parsed_ce_config, "message", "this is a message");
+}
--- src/vcpkg/configuration.cpp
+++ src/vcpkg/configuration.cpp
@@ -9,17 +9,59 @@ namespace
 {
     using namespace vcpkg;
 
+    struct DictionaryDeserializer final : Json::IDeserializer<Json::Object>
+    {
+        virtual StringView type_name() const override { return "an artifact-reference object"; }
+
+        virtual Optional<Json::Object> visit_object(Json::Reader& r, const Json::Object& obj) override;
+
+        static DictionaryDeserializer instance;
+    };
+    DictionaryDeserializer DictionaryDeserializer::instance;
+
+    struct CeConfigurationDeserializer final : Json::IDeserializer<Json::Object>
+    {
+        virtual StringView type_name() const override { return "a ce configuration object"; }
+
+        constexpr static StringLiteral CE_ERROR = "error";
+        constexpr static StringLiteral CE_WARNING = "warning";
+        constexpr static StringLiteral CE_MESSAGE = "message";
+        constexpr static StringLiteral CE_APPLY = "apply";
+        constexpr static StringLiteral CE_SETTINGS = "settings";
+        constexpr static StringLiteral CE_REQUIRES = "requires";
+        constexpr static StringLiteral CE_SEE_ALSO = "see-also";
+        constexpr static StringLiteral CE_DEMANDS = "demands";
+
+        virtual Optional<Json::Object> visit_object(Json::Reader& r, const Json::Object& obj) override;
+
+        static CeConfigurationDeserializer instance;
+    };
+    CeConfigurationDeserializer CeConfigurationDeserializer::instance;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_ERROR;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_WARNING;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_MESSAGE;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_APPLY;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_SETTINGS;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_REQUIRES;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_SEE_ALSO;
+    constexpr StringLiteral CeConfigurationDeserializer::CE_DEMANDS;
+
+    struct DemandsDeserializer final : Json::IDeserializer<Json::Object>
+    {
+        virtual StringView type_name() const override { return "a ce demand object"; }
+
+        virtual Optional<Json::Object> visit_object(Json::Reader& r, const Json::Object& obj) override;
+
+        static DemandsDeserializer instance;
+    };
+    DemandsDeserializer DemandsDeserializer::instance;
+
     struct ConfigurationDeserializer final : Json::IDeserializer<Configuration>
     {
         virtual StringView type_name() const override { return "a configuration object"; }
 
         constexpr static StringLiteral DEFAULT_REGISTRY = "default-registry";
         constexpr static StringLiteral REGISTRIES = "registries";
-        virtual View<StringView> valid_fields() const override
-        {
-            constexpr static StringView t[] = {DEFAULT_REGISTRY, REGISTRIES};
-            return t;
-        }
 
         virtual Optional<Configuration> visit_object(Json::Reader& r, const Json::Object& obj) override;
 
@@ -28,12 +70,117 @@ namespace
     private:
         Path configuration_directory;
     };
-
     constexpr StringLiteral ConfigurationDeserializer::DEFAULT_REGISTRY;
     constexpr StringLiteral ConfigurationDeserializer::REGISTRIES;
 
+    Optional<Json::Object> DictionaryDeserializer::visit_object(Json::Reader& r, const Json::Object& obj)
+    {
+        Json::Object ret;
+        for (const auto& el : obj)
+        {
+            auto key = el.first;
+            auto value = el.second;
+
+            if (!value.is_string())
+            {
+                r.add_generic_error(type_name(), "All elements must be key/value pairs");
+                continue;
+            }
+
+            ret.insert_or_replace(el.first.to_string(), el.second);
+        }
+        return ret;
+    }
+
+    Optional<Json::Object> CeConfigurationDeserializer::visit_object(Json::Reader& r, const Json::Object& obj)
+    {
+        auto extract_string = [&](const Json::Object& obj, StringView key, Json::Object& put_into) {
+            static Json::StringDeserializer string_deserializer{"a string"};
+
+            std::string value;
+            if (r.optional_object_field(obj, key, value, string_deserializer))
+            {
+                put_into.insert_or_replace(key.to_string(), Json::Value::string(value));
+            }
+        };
+        auto extract_dictionary = [&](const Json::Object& obj, StringView key, Json::Object& put_into) {
+            Json::Object value;
+            if (r.optional_object_field(obj, key, value, DictionaryDeserializer::instance))
+            {
+                put_into.insert_or_replace(key.to_string(), value);
+            }
+        };
+
+        Json::Object ret;
+        extract_string(obj, CE_ERROR, ret);
+        extract_string(obj, CE_WARNING, ret);
+        extract_string(obj, CE_MESSAGE, ret);
+        if (auto ce_apply = obj.get(CE_APPLY))
+        {
+            if (!ce_apply->is_object())
+            {
+                r.add_generic_error(type_name(), CE_APPLY, " must be an object");
+            }
+            else
+            {
+                ret.insert_or_replace(CE_APPLY, *ce_apply);
+            }
+        }
+        extract_dictionary(obj, CE_SETTINGS, ret);
+        extract_dictionary(obj, CE_REQUIRES, ret);
+        extract_dictionary(obj, CE_SEE_ALSO, ret);
+
+        Json::Object demands;
+        if (r.optional_object_field(obj, CE_DEMANDS, demands, DemandsDeserializer::instance))
+        {
+            ret.insert_or_replace(CE_DEMANDS, demands);
+        }
+        return ret;
+    }
+
+    Optional<Json::Object> DemandsDeserializer::visit_object(Json::Reader& r, const Json::Object& obj)
+    {
+        Json::Object ret;
+        for (const auto& el : obj)
+        {
+            auto filter = el.first.to_string();
+            // if (Strings::starts_with(filter, "$"))
+            //{
+            //    continue;
+            //}
+
+            if (!el.second.is_object())
+            {
+                r.add_generic_error(type_name(), filter, " must be an object");
+                continue;
+            }
+
+            auto maybe_demand = r.visit(el.second, CeConfigurationDeserializer::instance);
+            if (maybe_demand.has_value())
+            {
+                ret.insert_or_replace(filter, maybe_demand.value_or_exit(VCPKG_LINE_INFO));
+            }
+        }
+        return ret;
+    }
+
     Optional<Configuration> ConfigurationDeserializer::visit_object(Json::Reader& r, const Json::Object& obj)
     {
+        static constexpr StringView known_fields[]{
+            ConfigurationDeserializer::DEFAULT_REGISTRY,
+            ConfigurationDeserializer::REGISTRIES,
+            CeConfigurationDeserializer::CE_APPLY,
+            CeConfigurationDeserializer::CE_DEMANDS,
+            CeConfigurationDeserializer::CE_ERROR,
+            CeConfigurationDeserializer::CE_MESSAGE,
+            CeConfigurationDeserializer::CE_REQUIRES,
+            CeConfigurationDeserializer::CE_SEE_ALSO,
+            CeConfigurationDeserializer::CE_SETTINGS,
+            CeConfigurationDeserializer::CE_WARNING,
+        };
+
+        static Json::StringDeserializer string_deserializer{"a string"};
+
         Json::Object extra_info;
         for (const auto& el : obj)
         {
@@ -62,7 +209,14 @@ namespace
             registries.add_registry(std::move(reg));
         }
 
-        return Configuration{std::move(registries), extra_info};
+        Json::Object ce_config_obj;
+        auto maybe_ce_config = r.visit(obj, CeConfigurationDeserializer::instance);
+        if (auto ce_config = maybe_ce_config.get())
+        {
+            ce_config_obj = *ce_config;
+        }
+
+        return Configuration{std::move(registries), ce_config_obj, extra_info};
     }
 
     ConfigurationDeserializer::ConfigurationDeserializer(const Path& configuration_directory)
@@ -104,6 +258,11 @@ namespace
                 reg_obj.insert(REGISTRY_PACKAGES, serialize_packages_list(reg.packages()));
                 reg_arr.push_back(std::move(reg_obj));
             }
+        }
+
+        for (const auto& el : config.ce_configuration)
+        {
+            obj.insert(el.first.to_string(), el.second);
         }
 
         return obj;
