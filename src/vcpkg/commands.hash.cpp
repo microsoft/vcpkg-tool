b3eafb884d6ae4fbfadce11cec731d1c0da306ea
Sergii Baitala <sbaitala@imprivata.com>
2021-11-17 15:43:50 +0200

    change path initialization

--- src/vcpkg/commands.hash.cpp
+++ src/vcpkg/commands.hash.cpp
@@ -20,8 +20,7 @@ namespace vcpkg::Commands::Hash
     {
         (void)args.parse_arguments(COMMAND_STRUCTURE);
 
-        Path file_to_hash = args.command_arguments[0];
-        if (file_to_hash.is_relative()) file_to_hash = (paths.original_cwd / file_to_hash).lexically_normal();
+        const auto file_to_hash = (paths.original_cwd / args.command_arguments[0]).lexically_normal();
 
         auto algorithm = vcpkg::Hash::Algorithm::Sha512;
         if (args.command_arguments.size() == 2)
