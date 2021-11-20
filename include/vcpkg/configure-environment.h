7fe8faecf9fe79c03b21c8c42f3eeed7867bd6d7
Billy Robert O'Neal III <bion@microsoft.com>
2021-11-19 17:12:48 -0800

    Add "add" command and fix bug where forwarded arguments include the selection command.

--- include/vcpkg/configure-environment.h
+++ include/vcpkg/configure-environment.h
@@ -6,6 +6,6 @@
 
 namespace vcpkg
 {
-    int run_configure_environment_command(const VcpkgPaths& paths, View<std::string> args = {});
-    int run_configure_environment_command(const VcpkgPaths& paths, StringView arg0, View<std::string> args = {});
+    int run_configure_environment_command(const VcpkgPaths& paths, View<std::string> args);
+    int run_configure_environment_command(const VcpkgPaths& paths, StringView arg0, View<std::string> args);
 }
