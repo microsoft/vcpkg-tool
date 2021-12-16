3588493ce8f8dcebcba5162d57c18a277a774662
nicole mazzuca <mazzucan@outlook.com>
2021-12-16 14:45:39 -0800

    [localization] Add support to checks.h for localized messages

--- include/vcpkg/base/checks.h
+++ include/vcpkg/base/checks.h
@@ -2,6 +2,7 @@
 
 #include <vcpkg/base/basic_checks.h>
 #include <vcpkg/base/strings.h>
+#include <vcpkg/base/messages.h>
 
 namespace vcpkg::Checks
 {
@@ -17,10 +18,17 @@ namespace vcpkg::Checks
         exit_with_message(line_info,
                           Strings::format(error_message_template, error_message_arg1, error_message_args...));
     }
+    template <class Message, class... Args>
+    [[noreturn]] typename Message::is_message_type exit_with_message(const LineInfo& line_info,
+                                        Message m,
+                                        const Args&... args)
+    {
+        exit_with_message(line_info, msg::format(m, args...));
+    }
 
     template<class Conditional, class Arg1, class... Args>
     void check_exit(const LineInfo& line_info,
-                    Conditional&& expression,
+                    const Conditional& expression,
                     const char* error_message_template,
                     const Arg1& error_message_arg1,
                     const Args&... error_message_args)
@@ -30,6 +38,18 @@ namespace vcpkg::Checks
             // Only create the string if the expression is false
             exit_with_message(line_info,
                               Strings::format(error_message_template, error_message_arg1, error_message_args...));
+        }
+    }
+    template<class Conditional, class Message, class... Args>
+    typename Message::is_message_type check_exit(const LineInfo& line_info,
+                    const Conditional& expression,
+                    Message m,
+                    const Args&... args)
+    {
+        if (!expression)
+        {
+            // Only create the string if the expression is false
+            exit_with_message(line_info, msg::format(m, args...));
         }
     }
 
@@ -42,10 +62,17 @@ namespace vcpkg::Checks
         exit_maybe_upgrade(line_info,
                            Strings::format(error_message_template, error_message_arg1, error_message_args...));
     }
+    template<class Message, class... Args>
+    [[noreturn]] typename Message::is_message_type exit_maybe_upgrade(const LineInfo& line_info,
+                                         Message m,
+                                         const Args&... args)
+    {
+        exit_maybe_upgrade(line_info, msg::format(m, args...));
+    }
 
     template<class Conditional, class Arg1, class... Args>
     void check_maybe_upgrade(const LineInfo& line_info,
-                             Conditional&& expression,
+                             const Conditional& expression,
                              const char* error_message_template,
                              const Arg1& error_message_arg1,
                              const Args&... error_message_args)
@@ -55,6 +82,18 @@ namespace vcpkg::Checks
             // Only create the string if the expression is false
             exit_maybe_upgrade(line_info,
                                Strings::format(error_message_template, error_message_arg1, error_message_args...));
+        }
+    }
+    template<class Conditional, class Message, class... Args>
+    typename Message::is_message_type check_maybe_upgrade(const LineInfo& line_info,
+                             const Conditional& expression,
+                             Message m,
+                             const Args&... args)
+    {
+        if (!expression)
+        {
+            // Only create the string if the expression is false
+            exit_maybe_upgrade(line_info, msg::format(m, args...));
         }
     }
 }
