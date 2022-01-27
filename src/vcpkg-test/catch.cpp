9c55995d428523d798a9ac669b0c35b4a9a35295
Robert Schumacher <roschuma@microsoft.com>
2022-01-27 02:27:42 +0000

    Address CR feedback

--- src/vcpkg-test/catch.cpp
+++ src/vcpkg-test/catch.cpp
@@ -7,6 +7,8 @@
 int main(int argc, char** argv)
 {
     if (vcpkg::get_environment_variable("VCPKG_DEBUG").value_or("") == "1") vcpkg::Debug::g_debugging = true;
+    // We set VCPKG_ROOT to an invalid value to ensure unit tests do not attempt to instantiate VcpkgRoot
+    vcpkg::set_environment_variable("VCPKG_ROOT", "VCPKG_TESTS_SHOULD_NOT_USE_VCPKG_ROOT");
 
     return Catch::Session().run(argc, argv);
 }
