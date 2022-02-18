e37439d8bacbf1b5eca3c7b5e479119208ef2c16
nicole mazzuca <mazzucan@outlook.com>
2022-02-17 18:08:23 -0800

    haha!

--- src/localization/Format-LocalizationFiles.ps1
+++ src/localization/Format-LocalizationFiles.ps1
@@ -4,6 +4,7 @@ $fileList = Get-Content $Env:XLocFileList
 
 $fileList | % {
     $fileName = $_
+    "Reading $fileName" | Out-Host
     $fileContents = Get-Content -Raw $fileName
     $fileContents = $fileContents -replace "`r`n","`n" # use LF, not CRLF
     if (-not $fileContents.EndsWith("`n"))
@@ -13,4 +14,5 @@ $fileList | % {
     # this (convert to UTF-8 followed by WriteAllBytes) avoids adding a BOM in Windows PowerShell
     $fileContents = [System.Text.Encoding]::UTF8.GetBytes($fileContents)
     [io.file]::WriteAllBytes($fileName, $fileContents)
+    "Wrote $fileName" | Out-Host
 }
