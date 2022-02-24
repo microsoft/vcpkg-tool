microsoft/vcpkg-tool

commit f3841c4f3e04885a73c4ab9db7b396b1e32f9cb6

Move toolchain decision making entirely into the tool (#350)
* Move toolchain decision making entirely into the tool

* Update src/vcpkg/build.cpp

Co-authored-by: Billy O'Neal <bion@microsoft.com>

Co-authored-by: Billy O'Neal <bion@microsoft.com>

src/vcpkg/build.cpp
@@ -764,6 +764,7 @@ namespace vcpkg::Build
             {"_VCPKG_DOWNLOAD_TOOL", to_string(action.build_options.download_tool)},
             {"_VCPKG_EDITABLE", Util::Enum::to_bool(action.build_options.editable) ? "1" : "0"},
             {"_VCPKG_NO_DOWNLOADS", !Util::Enum::to_bool(action.build_options.allow_downloads) ? "1" : "0"},
+            {"VCPKG_CHAINLOAD_TOOLCHAIN_FILE", action.pre_build_info(VCPKG_LINE_INFO).toolchain_file()},
         };
 
         if (action.build_options.download_tool == DownloadTool::ARIA2)
@@ -853,7 +854,21 @@ namespace vcpkg::Build
         {
             return m_paths.scripts / "toolchains/mingw.cmake";
         }
-        else if (cmake_system_name.empty() || cmake_system_name == "Windows" || cmake_system_name == "WindowsStore")
+        else if (cmake_system_name == "WindowsStore")
+        {
+            // HACK: remove once we have fully shipped a uwp toolchain
+            static bool have_uwp_triplet =
+                m_paths.get_filesystem().exists(m_paths.scripts / "toolchains/uwp.cmake", IgnoreErrors{});
+            if (have_uwp_triplet)
+            {
+                return m_paths.scripts / "toolchains/uwp.cmake";
+            }
+            else
+            {
+                return m_paths.scripts / "toolchains/windows.cmake";
+            }
+        }
+        else if (cmake_system_name.empty() || cmake_system_name == "Windows")
         {
             return m_paths.scripts / "toolchains/windows.cmake";
         }

commit ccc90b0411798a8d65ba1f9de3ec08a76d98c29d

Add Z_ to VCPKG_CHAINLOAD_TOOLCHAIN_FILE to workaround the regression caused by #350

src/vcpkg/build.cpp
@@ -765,7 +765,7 @@ namespace vcpkg::Build
             {"_VCPKG_DOWNLOAD_TOOL", to_string(action.build_options.download_tool)},
             {"_VCPKG_EDITABLE", Util::Enum::to_bool(action.build_options.editable) ? "1" : "0"},
             {"_VCPKG_NO_DOWNLOADS", !Util::Enum::to_bool(action.build_options.allow_downloads) ? "1" : "0"},
-            {"VCPKG_CHAINLOAD_TOOLCHAIN_FILE", action.pre_build_info(VCPKG_LINE_INFO).toolchain_file()},
+            {"Z_VCPKG_CHAINLOAD_TOOLCHAIN_FILE", action.pre_build_info(VCPKG_LINE_INFO).toolchain_file()},
         };
 
         if (action.build_options.download_tool == DownloadTool::ARIA2)