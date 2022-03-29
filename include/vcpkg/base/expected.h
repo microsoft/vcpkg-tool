fdcd52c6d2599fe92c1ec8f6dadace708ccf01a3
Billy Robert O'Neal III <bion@microsoft.com>
2022-03-29 16:25:59 -0700

    Fix copy pasto in https://github.com/microsoft/vcpkg-tool/pull/467

--- include/vcpkg/base/expected.h
+++ include/vcpkg/base/expected.h
@@ -134,13 +134,13 @@ namespace vcpkg
         // Constructors are intentionally implicit
 
         ExpectedT(const S& s, ExpectedRightTag = {}) : m_s(s) { }
-        template<std::enable_if_t<!std::is_reference<S>::value, int> = 0>
+        template<class U = S, std::enable_if_t<!std::is_reference<U>::value, int> = 0>
         ExpectedT(S&& s, ExpectedRightTag = {}) : m_s(std::move(s))
         {
         }
 
         ExpectedT(const T& t, ExpectedLeftTag = {}) : m_t(t) { }
-        template<std::enable_if_t<!std::is_reference<S>::value, int> = 0>
+        template<class U = T, std::enable_if_t<!std::is_reference<U>::value, int> = 0>
         ExpectedT(T&& t, ExpectedLeftTag = {}) : m_t(std::move(t))
         {
         }
