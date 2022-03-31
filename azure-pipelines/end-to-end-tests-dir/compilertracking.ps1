2b29d448f5310ef4f255db1dd8527533e9928561
Billy O'Neal <bion@microsoft.com>
2022-03-31 14:08:32 -0700

    Add hello world ports to use in testing; insulates compiler tracking e2e tests from SourceForge. (#475)

--- azure-pipelines/end-to-end-tests-dir/compilertracking.ps1
+++ azure-pipelines/end-to-end-tests-dir/compilertracking.ps1
@@ -7,25 +7,25 @@ if ($Triplet -ne "x64-linux") {
 $args = $commonArgs + @("--overlay-triplets=$PSScriptRoot/../e2e_ports/compilertracking", "--binarysource=clear;files,$ArchiveRoot,readwrite")
 
 # Test simple installation
-Run-Vcpkg -TestArgs ($args + @("install", "tinyxml"))
+Run-Vcpkg -TestArgs ($args + @("install", "vcpkg-hello-world-1"))
 Throw-IfFailed
-if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/tinyxml/vcpkg_abi_info.txt")) {
-    throw "Expected tinyxml to perform compiler detection"
+if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/vcpkg-hello-world-1/vcpkg_abi_info.txt")) {
+    throw "Expected vcpkg-hello-world-1 to perform compiler detection"
 }
 Remove-Item -Recurse -Force $installRoot
 
-Run-Vcpkg -TestArgs ($args + @("install", "rapidjson"))
+Run-Vcpkg -TestArgs ($args + @("install", "vcpkg-hello-world-2"))
 Throw-IfFailed
-if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/rapidjson/vcpkg_abi_info.txt")) {
-    throw "Expected rapidjson to not perform compiler detection"
+if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/vcpkg-hello-world-2/vcpkg_abi_info.txt")) {
+    throw "Expected vcpkg-hello-world-2 to not perform compiler detection"
 }
 Remove-Item -Recurse -Force $installRoot
 
-Run-Vcpkg -TestArgs ($args + @("install", "rapidjson", "tinyxml"))
+Run-Vcpkg -TestArgs ($args + @("install", "vcpkg-hello-world-2", "vcpkg-hello-world-1"))
 Throw-IfFailed
-if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/tinyxml/vcpkg_abi_info.txt")) {
-    throw "Expected tinyxml to perform compiler detection"
+if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/vcpkg-hello-world-1/vcpkg_abi_info.txt")) {
+    throw "Expected vcpkg-hello-world-1 to perform compiler detection"
 }
-if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/rapidjson/vcpkg_abi_info.txt")) {
-    throw "Expected rapidjson to not perform compiler detection"
+if (-Not (select-string "^triplet_abi [0-9a-f]+-[0-9a-f]+$" "$installRoot/x64-linux/share/vcpkg-hello-world-2/vcpkg_abi_info.txt")) {
+    throw "Expected vcpkg-hello-world-2 to not perform compiler detection"
 }
