microsoft/vcpkg-tool

commit 0840ffdf10c20c23fc5b9a048e1d7f896e3e76b1

[telemetry] Add additional context for some commands (#486)
* Log `add` subcommands in telemetry

* Add context for find command

* Track use of overlay ports

* add more info to insert(existing-key)

* oops

* Change tracking for overlay ports

* Add `track_option(name, bool)` and `track_property(name, bool)`

Co-authored-by: nicole mazzuca <mazzucan@outlook.com>

src/vcpkg/commands.add.cpp
@@ -1,4 +1,5 @@
 #include <vcpkg/base/basic_checks.h>
+#include <vcpkg/base/hash.h>
 #include <vcpkg/base/messages.h>
 #include <vcpkg/base/strings.h>
 #include <vcpkg/base/system.print.h>
@@ -6,6 +7,7 @@
 #include <vcpkg/commands.add.h>
 #include <vcpkg/configure-environment.h>
 #include <vcpkg/documentation.h>
+#include <vcpkg/metrics.h>
 #include <vcpkg/paragraphs.h>
 #include <vcpkg/vcpkgcmdarguments.h>
 #include <vcpkg/vcpkgpaths.h>
@@ -45,22 +47,29 @@ namespace
                                  (msg::command_line),
                                  "",
                                  "'{command_line}' can only add one artifact at a time.");
-
 }
 
 namespace vcpkg::Commands
 {
     void AddCommand::perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths) const
     {
-        args.parse_arguments(AddCommandStructure);
+        (void)args.parse_arguments(AddCommandStructure);
         auto&& selector = args.command_arguments[0];
+
         if (selector == "artifact")
         {
             Checks::msg_check_exit(VCPKG_LINE_INFO,
                                    args.command_arguments.size() <= 2,
                                    msgAddArtifactOnlyOne,
                                    msg::command_line = "vcpkg add artifact");
-            std::string ce_args[] = {"add", args.command_arguments[1]};
+
+            auto artifact_name = args.command_arguments[1];
+
+            auto metrics = LockGuardPtr<Metrics>(g_metrics);
+            metrics->track_property("command_context", "artifact");
+            metrics->track_property("command_args", Hash::get_string_hash(artifact_name, Hash::Algorithm::Sha256));
+
+            std::string ce_args[] = {"add", artifact_name};
             Checks::exit_with_code(VCPKG_LINE_INFO, run_configure_environment_command(paths, ce_args));
         }
 
@@ -126,6 +135,14 @@ namespace vcpkg::Commands
             paths.get_filesystem().write_contents(
                 manifest_path, Json::stringify(serialize_manifest(manifest_scf), {}), VCPKG_LINE_INFO);
             msg::println(msgAddPortSucceded);
+
+            auto metrics = LockGuardPtr<Metrics>(g_metrics);
+            metrics->track_property("command_context", "port");
+            metrics->track_property("command_args",
+                                    Strings::join(" ", Util::fmap(specs, [](auto&& spec) -> std::string {
+                                                      return Hash::get_string_hash(spec.name, Hash::Algorithm::Sha256);
+                                                  })));
+
             Checks::exit_success(VCPKG_LINE_INFO);
         }
 

src/vcpkg/commands.find.cpp
@@ -1,3 +1,4 @@
+#include <vcpkg/base/hash.h>
 #include <vcpkg/base/system.print.h>
 
 #include <vcpkg/commands.find.h>
@@ -5,6 +6,7 @@
 #include <vcpkg/dependencies.h>
 #include <vcpkg/globalstate.h>
 #include <vcpkg/help.h>
+#include <vcpkg/metrics.h>
 #include <vcpkg/paragraphs.h>
 #include <vcpkg/portfileprovider.h>
 #include <vcpkg/sourceparagraph.h>
@@ -224,11 +226,27 @@ namespace vcpkg::Commands
                 print2(Color::warning, "--x-json has no effect on find artifact\n");
             }
 
+            auto metrics = LockGuardPtr<Metrics>(g_metrics);
+            metrics->track_property("command_context", "artifact");
+            if (filter.has_value())
+            {
+                metrics->track_property(
+                    "command_args",
+                    Hash::get_string_hash(filter.value_or_exit(VCPKG_LINE_INFO), Hash::Algorithm::Sha256));
+            }
             perform_find_artifact_and_exit(paths, filter);
         }
 
         if (selector == "port")
         {
+            auto metrics = LockGuardPtr<Metrics>(g_metrics);
+            metrics->track_property("command_context", "port");
+            if (filter.has_value())
+            {
+                metrics->track_property(
+                    "command_args",
+                    Hash::get_string_hash(filter.value_or_exit(VCPKG_LINE_INFO), Hash::Algorithm::Sha256));
+            }
             perform_find_port_and_exit(paths, full_description, enable_json, filter, args.overlay_ports);
         }
 

include/vcpkg/metrics.h
@@ -23,7 +23,9 @@ namespace vcpkg
         void track_metric(const std::string& name, double value);
         void track_buildtime(const std::string& name, double value);
         void track_property(const std::string& name, const std::string& value);
+        void track_property(const std::string& name, bool value);
         void track_feature(const std::string& feature, bool value);
+        void track_option(const std::string& option, bool value);
 
         bool metrics_enabled();
 

src/vcpkg.cpp
@@ -85,6 +85,8 @@ static void inner(vcpkg::Filesystem& fs, const VcpkgCmdArguments& args)
         }
     };
 
+    LockGuardPtr<Metrics>(g_metrics)->track_option("overlay_ports", !args.overlay_ports.empty());
+
     if (const auto command_function = find_command(Commands::get_available_basic_commands()))
     {
         LockGuardPtr<Metrics>(g_metrics)->track_property("command_name", command_function->name);

src/vcpkg/base/json.cpp
@@ -350,13 +350,13 @@ namespace vcpkg::Json
     Value& Object::insert(StringView key, std::string&& value) { return insert(key, Value::string(std::move(value))); }
     Value& Object::insert(StringView key, Value&& value)
     {
-        vcpkg::Checks::check_exit(VCPKG_LINE_INFO, !contains(key));
+        vcpkg::Checks::check_exit(VCPKG_LINE_INFO, !contains(key), "key '%s' already exists in object", key);
         underlying_.emplace_back(key.to_string(), std::move(value));
         return underlying_.back().second;
     }
     Value& Object::insert(StringView key, const Value& value)
     {
-        vcpkg::Checks::check_exit(VCPKG_LINE_INFO, !contains(key));
+        vcpkg::Checks::check_exit(VCPKG_LINE_INFO, !contains(key), "key '%s' already exists in object", key);
         underlying_.emplace_back(key.to_string(), value);
         return underlying_.back().second;
     }

src/vcpkg/install.cpp
@@ -915,7 +915,7 @@ namespace vcpkg::Install
                                                  ? Dependencies::UnsupportedPortAction::Warn
                                                  : Dependencies::UnsupportedPortAction::Error;
 
-        LockGuardPtr<Metrics>(g_metrics)->track_feature("install_manifest_mode", paths.manifest_mode_enabled());
+        LockGuardPtr<Metrics>(g_metrics)->track_property("install_manifest_mode", paths.manifest_mode_enabled());
 
         if (paths.manifest_mode_enabled())
         {

src/vcpkg/metrics.cpp
@@ -161,6 +161,10 @@ namespace vcpkg
         {
             properties.insert(Strings::concat("feature-flag-", name), Json::Value::boolean(value));
         }
+        void track_option(StringView name, bool value)
+        {
+            properties.insert(Strings::concat("option_", name), Json::Value::boolean(value));
+        }
 
         std::string format_event_data_template() const
         {
@@ -328,7 +332,17 @@ namespace vcpkg
         g_metricmessage.track_property(name, value);
     }
 
+    void Metrics::track_property(const std::string& name, bool value)
+    {
+        if (value)
+        {
+            g_metricmessage.track_property(name, "defined");
+        }
+    }
+
     void Metrics::track_feature(const std::string& name, bool value) { g_metricmessage.track_feature(name, value); }
+
+    void Metrics::track_option(const std::string& name, bool value) { g_metricmessage.track_option(name, value); }
 
     void Metrics::upload(const std::string& payload)
     {

commit 1b0d9ed2927add19d51666143625c84467ad546e

Fix crash-on-shutdown around all "CE" powered commands introduced in https://github.com/microsoft/vcpkg-tool/commit/0840ffdf10c20c23fc5b9a048e1d7f896e3e76b1
These commands called into CE bits, and exit_with_code, while holding the lock. This causes an immediate crash on shutdown because the lock is reentered. This change ensures that a minimal amount of code runs while holding the lock.

src/vcpkg/commands.add.cpp
@@ -64,10 +64,12 @@ namespace vcpkg::Commands
                                    msg::command_line = "vcpkg add artifact");
 
             auto artifact_name = args.command_arguments[1];
-
-            auto metrics = LockGuardPtr<Metrics>(g_metrics);
-            metrics->track_property("command_context", "artifact");
-            metrics->track_property("command_args", Hash::get_string_hash(artifact_name, Hash::Algorithm::Sha256));
+            auto artifact_hash = Hash::get_string_hash(artifact_name, Hash::Algorithm::Sha256);
+            {
+                auto metrics = LockGuardPtr<Metrics>(g_metrics);
+                metrics->track_property("command_context", "artifact");
+                metrics->track_property("command_args", artifact_hash);
+            } // unlock g_metrics
 
             std::string ce_args[] = {"add", artifact_name};
             Checks::exit_with_code(VCPKG_LINE_INFO, run_configure_environment_command(paths, ce_args));
@@ -135,12 +137,14 @@ namespace vcpkg::Commands
                 manifest->path, Json::stringify(serialize_manifest(manifest_scf), {}), VCPKG_LINE_INFO);
             msg::println(msgAddPortSucceded);
 
-            auto metrics = LockGuardPtr<Metrics>(g_metrics);
-            metrics->track_property("command_context", "port");
-            metrics->track_property("command_args",
-                                    Strings::join(" ", Util::fmap(specs, [](auto&& spec) -> std::string {
-                                                      return Hash::get_string_hash(spec.name, Hash::Algorithm::Sha256);
-                                                  })));
+            auto command_args_hash = Strings::join(" ", Util::fmap(specs, [](auto&& spec) -> std::string {
+                                                       return Hash::get_string_hash(spec.name, Hash::Algorithm::Sha256);
+                                                   }));
+            {
+                auto metrics = LockGuardPtr<Metrics>(g_metrics);
+                metrics->track_property("command_context", "port");
+                metrics->track_property("command_args", command_args_hash);
+            } // unlock metrics
 
             Checks::exit_success(VCPKG_LINE_INFO);
         }

src/vcpkg/commands.find.cpp
@@ -226,27 +226,31 @@ namespace vcpkg::Commands
                 print2(Color::warning, "--x-json has no effect on find artifact\n");
             }
 
-            auto metrics = LockGuardPtr<Metrics>(g_metrics);
-            metrics->track_property("command_context", "artifact");
-            if (filter.has_value())
+            auto args_hash = Hash::get_string_hash(filter.value_or_exit(VCPKG_LINE_INFO), Hash::Algorithm::Sha256);
             {
-                metrics->track_property(
-                    "command_args",
-                    Hash::get_string_hash(filter.value_or_exit(VCPKG_LINE_INFO), Hash::Algorithm::Sha256));
-            }
+                auto metrics = LockGuardPtr<Metrics>(g_metrics);
+                metrics->track_property("command_context", "artifact");
+                if (filter.has_value())
+                {
+                    metrics->track_property("command_args", args_hash);
+                }
+            } // unlock metrics
+
             perform_find_artifact_and_exit(paths, filter);
         }
 
         if (selector == "port")
         {
-            auto metrics = LockGuardPtr<Metrics>(g_metrics);
-            metrics->track_property("command_context", "port");
-            if (filter.has_value())
+            auto filter_hash = Hash::get_string_hash(filter.value_or_exit(VCPKG_LINE_INFO), Hash::Algorithm::Sha256);
             {
-                metrics->track_property(
-                    "command_args",
-                    Hash::get_string_hash(filter.value_or_exit(VCPKG_LINE_INFO), Hash::Algorithm::Sha256));
+                auto metrics = LockGuardPtr<Metrics>(g_metrics);
+                metrics->track_property("command_context", "port");
+                if (filter.has_value())
+                {
+                    metrics->track_property("command_args", filter_hash);
+                }
             }
+
             perform_find_port_and_exit(paths, full_description, enable_json, filter, args.overlay_ports);
         }
 