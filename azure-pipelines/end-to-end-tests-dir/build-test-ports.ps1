44b6add70c67303ef26d965b3853fd12bbd11c2d
Alexander Neumann <you@example.com>
2022-05-04 15:26:43 +0200

    add 'if ($IsWindows)' and install another port for testing

--- azure-pipelines/end-to-end-tests-dir/build-test-ports.ps1
+++ azure-pipelines/end-to-end-tests-dir/build-test-ports.ps1
@@ -3,5 +3,8 @@
 $CurrentTest = "Build Test Ports"
 
 Run-Vcpkg --overlay-ports="$PSScriptRoot/../e2e_ports/overlays" install vcpkg-empty-port
-Run-Vcpkg --overlay-ports="$PSScriptRoot/../e2e_ports" install vcpkg-find-acquire-program
+Run-Vcpkg --overlay-ports="$PSScriptRoot/../e2e_ports" install vcpkg-internal-e2e-test-port
+if ($IsWindows) {
+    Run-Vcpkg --overlay-ports="$PSScriptRoot/../e2e_ports" install vcpkg-find-acquire-program
+}
 Throw-IfFailed
