969ae1879586c3a41389f8ae4da485408e22de0f
Billy Robert O'Neal <bion@microsoft.com>
2022-06-06 13:45:17 -0700

    Use the rejection channel for fetch failure.

--- ce/ce/vcpkg.ts
+++ ce/ce/vcpkg.ts
@@ -1,40 +1,39 @@
 import { spawn } from "child_process";
+import { i } from "./i18n";
 import { Session } from "./session";
 
 /** @internal */
 export class Vcpkg {
-    constructor(private readonly session: Session) {}
+    constructor(private readonly session: Session) { }
 
-    fetch(fetchKey: string): Promise<string | undefined> {
-        return this.runVcpkg(['fetch', fetchKey, '--x-stderr-status']).then((results) => {
-            if (results === undefined) {
-                if (fetchKey === 'git') {
-                    this.session.channels.warning('failed to fetch git, falling back to attempting to use git from the PATH');
-                    return 'git';
-                }
-
-                return results;
+    fetch(fetchKey: string): Promise<string> {
+        return this.runVcpkg(['fetch', fetchKey, '--x-stderr-status']).then((output) => {
+            return output.trimEnd();
+        }, (error) => {
+            if (fetchKey === 'git') {
+                this.session.channels.warning('failed to fetch git, falling back to attempting to use git from the PATH');
+                return Promise.resolve('git');
             }
 
-            return results.trimEnd();
+            return Promise.reject(error);
         });
     }
 
-    private runVcpkg(args: string[]): Promise<string | undefined> {
+    private runVcpkg(args: string[]): Promise<string> {
         return new Promise((accept, reject) => {
             if (!this.session.vcpkgCommand) {
-                accept(undefined);
+                reject(i`VCPKG_COMMAND was not set`);
                 return;
             }
 
-            const subproc = spawn(this.session.vcpkgCommand, args, {stdio: ['ignore', 'pipe', 'pipe']});
+            const subproc = spawn(this.session.vcpkgCommand, args, { stdio: ['ignore', 'pipe', 'pipe'] });
             let result = '';
             subproc.stdout.on('data', (chunk) => { result += chunk; });
             subproc.stderr.pipe(process.stdout);
             subproc.on('error', (err) => { reject(err); });
-            subproc.on('close', (code, signal) => {
+            subproc.on('close', (code: number, signal) => {
                 if (code === 0) { accept(result); }
-                accept(undefined);
+                reject(i`Running vcpkg internally returned a nonzero exit code: ${code}`);
             });
         });
     }
