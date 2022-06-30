d07adcff53f2f059b068202c5133a9b93951a6be
Billy O'Neal <bion@microsoft.com>
2022-06-29 22:24:58 -0700

    Add output asserts.

--- azure-pipelines/end-to-end-tests-dir/build-test-ports.ps1
+++ azure-pipelines/end-to-end-tests-dir/build-test-ports.ps1
@@ -8,7 +8,7 @@ Throw-IfFailed
 $output = Run-Vcpkg @commonArgs --overlay-ports="$PSScriptRoot/../e2e_ports/vcpkg-internal-e2e-test-port2" install vcpkg-internal-e2e-test-port2
 $output
 Throw-IfFailed
-if ($output -match "vcpkg-internal-e2e-test-port3") {
+if ($output -match 'vcpkg-internal-e2e-test-port3') {
     throw "Should not emit messages about -port3 while checking -port2"
 }
 
@@ -21,7 +21,14 @@ if ($IsWindows) {
     Throw-IfFailed
 }
 
-Run-Vcpkg @commonArgs --overlay-ports="$PSScriptRoot/../e2e_ports/overlays" install broken-no-name
+$output = Run-Vcpkg @commonArgs --overlay-ports="$PSScriptRoot/../e2e_ports/overlays" install broken-no-name
 Throw-IfNotFailed
-Run-Vcpkg @commonArgs --overlay-ports="$PSScriptRoot/../e2e_ports/overlays" install broken-no-version
+if (-not $output -match 'missing field') {
+    throw 'Did not detect missing field'
+}
+
+$output = Run-Vcpkg @commonArgs --overlay-ports="$PSScriptRoot/../e2e_ports/overlays" install broken-no-version
 Throw-IfNotFailed
+if (-not $output -match 'missing field') {
+    throw 'Did not detect missing field'
+}
