microsoft/vcpkg-tool

commit 107754ff90120f28417a30e86e56048e4884fd44

Localize commands3 (#642)
* FailedToParseManifest

FailedToReadParagraph
MissingArgFormatManifest
FailedToRemoveControl
FailedToWriteManifest

* FailedToParseManifest

FailedToReadParagraph
FailedToParseControl
FailedToWriteManifest
FailedToRemoveControl

* ManifestFormatCompleted

* replace deprecated call to print2

* Replacing printf with msg::write_unlocalized_text_to_stdout

* ArtifactsOptionIncompatibility

* AddCommandFirstArg

* FailedToObtainLocalPortGitSha

* replace printf statements

* [Add Function] LocalizedString& append_nl()

ErrorsFound
SuggestResolution

* Replaced calls to append_raw("\n") with append_nl()

* Sorted messages

* removed append_nl function

* Nitpicks:

* Change the use of FailedToObtainLocalPortGitSha, FailedToWriteManifest, FailedToRemoveControl, FailedToFormatMissingFile to print the error prefix.
* Add missing : in FailedToParseJson.
* Delete now-unused JsonErrorFailedToParse and JsonErrorFailedToRead.
* Words nitpick on MissingArgFormatManifest (was preexisting).
* Change ControlAndManifestFilesPresent to check and add the error prefix rather than using a check form.

Co-authored-by: Billy O'Neal <bion@microsoft.com>

src/vcpkg/base/messages.cpp
@@ -406,6 +406,7 @@ namespace vcpkg
     MessageSink& stderr_sink = stderr_sink_instance;
 
     REGISTER_MESSAGE(AddArtifactOnlyOne);
+    REGISTER_MESSAGE(AddCommandFirstArg);
     REGISTER_MESSAGE(AddFirstArgument);
     REGISTER_MESSAGE(AddingCompletionEntry);
     REGISTER_MESSAGE(AddPortRequiresManifest);
@@ -443,6 +444,7 @@ namespace vcpkg
     REGISTER_MESSAGE(AlreadyInstalledNotHead);
     REGISTER_MESSAGE(AnotherInstallationInProgress);
     REGISTER_MESSAGE(AppliedUserIntegration);
+    REGISTER_MESSAGE(ArtifactsOptionIncompatibility);
     REGISTER_MESSAGE(AssetSourcesArg);
     REGISTER_MESSAGE(AttemptingToFetchPackagesFromVendor);
     REGISTER_MESSAGE(AuthenticationMayRequireManualAction);
@@ -486,6 +488,7 @@ namespace vcpkg
     REGISTER_MESSAGE(CommandFailed);
     REGISTER_MESSAGE(CompressFolderFailed);
     REGISTER_MESSAGE(ConflictingValuesForOption);
+    REGISTER_MESSAGE(ControlAndManifestFilesPresent);
     REGISTER_MESSAGE(CopyrightIsDir);
     REGISTER_MESSAGE(CouldNotDeduceNugetIdAndVersion);
     REGISTER_MESSAGE(CreatedNuGetPackage);
@@ -518,6 +521,7 @@ namespace vcpkg
     REGISTER_MESSAGE(ErrorParsingBinaryParagraph);
     REGISTER_MESSAGE(ErrorRequireBaseline);
     REGISTER_MESSAGE(ErrorRequirePackagesList);
+    REGISTER_MESSAGE(ErrorsFound);
     REGISTER_MESSAGE(ErrorUnableToDetectCompilerInfo);
     REGISTER_MESSAGE(ErrorVcvarsUnsupported);
     REGISTER_MESSAGE(ErrorVsCodeNotFound);
@@ -533,12 +537,21 @@ namespace vcpkg
     REGISTER_MESSAGE(ExpectedValueForOption);
     REGISTER_MESSAGE(ExtendedDocumentationAtUrl);
     REGISTER_MESSAGE(FailedToExtract);
+    REGISTER_MESSAGE(FailedToFormatMissingFile);
+    REGISTER_MESSAGE(FailedToObtainLocalPortGitSha);
     REGISTER_MESSAGE(FailedToParseBinParagraph);
     REGISTER_MESSAGE(FailedToParseCMakeConsoleOut);
+    REGISTER_MESSAGE(FailedToParseControl);
+    REGISTER_MESSAGE(FailedToParseJson);
+    REGISTER_MESSAGE(FailedToParseManifest);
     REGISTER_MESSAGE(FailedToProvisionCe);
+    REGISTER_MESSAGE(FailedToRead);
+    REGISTER_MESSAGE(FailedToReadParagraph);
+    REGISTER_MESSAGE(FailedToRemoveControl);
     REGISTER_MESSAGE(FailedToRunToolToDetermineVersion);
     REGISTER_MESSAGE(FailedToStoreBackToMirror);
     REGISTER_MESSAGE(FailedToStoreBinaryCache);
+    REGISTER_MESSAGE(FailedToWriteManifest);
     REGISTER_MESSAGE(FailedVendorAuthentication);
     REGISTER_MESSAGE(FeedbackAppreciated);
     REGISTER_MESSAGE(FishCompletion);
@@ -609,8 +622,6 @@ namespace vcpkg
     REGISTER_MESSAGE(InvalidFilename);
     REGISTER_MESSAGE(InvalidFormatString);
     REGISTER_MESSAGE(InvalidLinkage);
-    REGISTER_MESSAGE(JsonErrorFailedToParse);
-    REGISTER_MESSAGE(JsonErrorFailedToRead);
     REGISTER_MESSAGE(JsonErrorMustBeAnObject);
     REGISTER_MESSAGE(JsonSwitch);
     REGISTER_MESSAGE(LaunchingProgramFailed);
@@ -637,10 +648,13 @@ namespace vcpkg
     REGISTER_MESSAGE(LoadingOverlayTriplet);
     REGISTER_MESSAGE(LocalizedMessageMustNotContainIndents);
     REGISTER_MESSAGE(LocalizedMessageMustNotEndWithNewline);
+    REGISTER_MESSAGE(ManifestFormatCompleted);
     REGISTER_MESSAGE(MismatchedBinaryParagraphs);
     REGISTER_MESSAGE(Missing7zHeader);
+    REGISTER_MESSAGE(MissingArgFormatManifest);
     REGISTER_MESSAGE(MissingExtension);
     REGISTER_MESSAGE(MissmatchedBinParagraphs);
+    REGISTER_MESSAGE(MissingPortSuggestPullRequest);
     REGISTER_MESSAGE(MonoInstructions);
     REGISTER_MESSAGE(MsiexecFailedToExtract);
     REGISTER_MESSAGE(MultiArch);
@@ -688,6 +702,8 @@ namespace vcpkg
     REGISTER_MESSAGE(SpecifyHostArch);
     REGISTER_MESSAGE(SpecifyTargetArch);
     REGISTER_MESSAGE(StoredBinaryCache);
+    REGISTER_MESSAGE(SuggestGitPull);
+    REGISTER_MESSAGE(SuggestResolution);
     REGISTER_MESSAGE(SuggestStartingBashShell);
     REGISTER_MESSAGE(SuggestUpdateVcpkg);
     REGISTER_MESSAGE(SupportedPort);

src/vcpkg/commands.civerifyversions.cpp
@@ -297,9 +297,9 @@ namespace vcpkg::Commands::CIVerifyVersions
         auto maybe_port_git_tree_map = paths.git_get_local_port_treeish_map();
         if (!maybe_port_git_tree_map)
         {
-            Checks::exit_with_message(VCPKG_LINE_INFO,
-                                      "Fatal error: Failed to obtain git SHAs for local ports.\n%s",
-                                      maybe_port_git_tree_map.error());
+            Checks::msg_exit_with_error(
+                VCPKG_LINE_INFO,
+                msg::format(msgFailedToObtainLocalPortGitSha).append_raw("\n" + maybe_port_git_tree_map.error()));
         }
 
         auto port_git_tree_map = maybe_port_git_tree_map.value_or_exit(VCPKG_LINE_INFO);
@@ -313,13 +313,13 @@ namespace vcpkg::Commands::CIVerifyVersions
             auto port_name = port_path.stem();
             if (Util::Sets::contains(exclusion_set, port_name.to_string()))
             {
-                if (verbose) vcpkg::printf("SKIP: %s\n", port_name);
+                if (verbose) msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("SKIP: %s\n", port_name));
                 continue;
             }
             auto git_tree_it = port_git_tree_map.find(port_name);
             if (git_tree_it == port_git_tree_map.end())
             {
-                vcpkg::printf(Color::error, "FAIL: %s\n", port_name);
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
                 errors.emplace(Strings::format("Error: While validating port %s.\n"
                                                "       Missing Git SHA.\n"
                                                "       Run:\n\n"
@@ -344,7 +344,7 @@ namespace vcpkg::Commands::CIVerifyVersions
 
             if (manifest_exists && control_exists)
             {
-                vcpkg::printf(Color::error, "FAIL: %s\n", port_name);
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
                 errors.emplace(
                     Strings::format("Error: While validating port %s.\n"
                                     "       Both a manifest file and a CONTROL file exist in port directory: %s",
@@ -355,7 +355,7 @@ namespace vcpkg::Commands::CIVerifyVersions
 
             if (!manifest_exists && !control_exists)
             {
-                vcpkg::printf(Color::error, "FAIL: %s\n", port_name);
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
                 errors.emplace(Strings::format("Error: While validating port %s.\n"
                                                "       No manifest file or CONTROL file exist in port directory: %s",
                                                port_name,
@@ -367,7 +367,7 @@ namespace vcpkg::Commands::CIVerifyVersions
             auto versions_file_path = paths.builtin_registry_versions / prefix / Strings::concat(port_name, ".json");
             if (!fs.exists(versions_file_path, IgnoreErrors{}))
             {
-                vcpkg::printf(Color::error, "FAIL: %s\n", port_name);
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
                 errors.emplace(Strings::format("Error: While validating port %s.\n"
                                                "       Missing expected versions file at: %s\n"
                                                "       Run:\n\n"
@@ -384,24 +384,29 @@ namespace vcpkg::Commands::CIVerifyVersions
 
             if (!maybe_ok)
             {
-                vcpkg::printf(Color::error, "FAIL: %s\n", port_name);
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
                 errors.emplace(maybe_ok.error());
                 continue;
             }
 
-            if (verbose) vcpkg::printf("%s", maybe_ok.value_or_exit(VCPKG_LINE_INFO));
+            if (verbose)
+                msg::write_unlocalized_text_to_stdout(Color::none,
+                                                      fmt::format("%s", maybe_ok.value_or_exit(VCPKG_LINE_INFO)));
         }
 
         if (!errors.empty())
         {
-            print2(Color::error, "Found the following errors:\n");
+            auto message = msg::format(msgErrorsFound);
             for (auto&& error : errors)
             {
-                vcpkg::printf(Color::error, "%s\n", error);
+                message.append_raw("\n").append_indent().append_raw(error);
             }
-            print2(Color::error,
-                   "\nTo attempt to resolve all errors at once, run:\n\n"
-                   "    vcpkg x-add-version --all\n\n");
+
+            message.append_raw("\n").append(
+                msgSuggestResolution, msg::command_name = "x-add-version", msg::option = "all");
+
+            msg::println_error(message);
+
             Checks::exit_fail(VCPKG_LINE_INFO);
         }
         Checks::exit_success(VCPKG_LINE_INFO);

src/vcpkg/commands.find.cpp
@@ -34,8 +34,7 @@ namespace
                 desc.push_back(Json::Value::string(line));
             }
         }
-
-        print2(Json::stringify(obj));
+        msg::write_unlocalized_text_to_stdout(Color::none, Json::stringify(obj));
     }
     constexpr const int s_name_and_ver_columns = 41;
     void do_print(const SourceParagraph& source_paragraph, bool full_desc)
@@ -43,10 +42,11 @@ namespace
         auto full_version = Version(source_paragraph.raw_version, source_paragraph.port_version).to_string();
         if (full_desc)
         {
-            vcpkg::printf("%-20s %-16s %s\n",
-                          source_paragraph.name,
-                          full_version,
-                          Strings::join("\n    ", source_paragraph.description));
+            msg::write_unlocalized_text_to_stdout(Color::none,
+                                                  fmt::format("%-20s %-16s %s\n",
+                                                              source_paragraph.name,
+                                                              full_version,
+                                                              Strings::join("\n    ", source_paragraph.description)));
         }
         else
         {
@@ -61,12 +61,13 @@ namespace
             used_columns += std::max<size_t>(full_version.size(), ver_size) + 1;
             size_t description_size = used_columns < (119 - 40) ? 119 - used_columns : 40;
 
-            vcpkg::printf("%-*s %-*s %s\n",
-                          name_columns,
-                          source_paragraph.name,
-                          ver_size,
-                          full_version,
-                          vcpkg::shorten_text(description, description_size));
+            msg::write_unlocalized_text_to_stdout(Color::none,
+                                                  fmt::format("%-*s %-*s %s\n",
+                                                              name_columns,
+                                                              source_paragraph.name,
+                                                              ver_size,
+                                                              full_version,
+                                                              vcpkg::shorten_text(description, description_size)));
         }
     }
 
@@ -75,7 +76,9 @@ namespace
         auto full_feature_name = Strings::concat(name, "[", feature_paragraph.name, "]");
         if (full_desc)
         {
-            vcpkg::printf("%-37s %s\n", full_feature_name, Strings::join("\n   ", feature_paragraph.description));
+            msg::write_unlocalized_text_to_stdout(
+                Color::none,
+                fmt::format("%-37s %s\n", full_feature_name, Strings::join("\n   ", feature_paragraph.description)));
         }
         else
         {
@@ -86,8 +89,11 @@ namespace
             }
             size_t desc_length =
                 119 - std::min<size_t>(60, 1 + std::max<size_t>(s_name_and_ver_columns, full_feature_name.size()));
-            vcpkg::printf(
-                "%-*s %s\n", s_name_and_ver_columns, full_feature_name, vcpkg::shorten_text(description, desc_length));
+            msg::write_unlocalized_text_to_stdout(Color::none,
+                                                  fmt::format("%-*s %s\n",
+                                                              s_name_and_ver_columns,
+                                                              full_feature_name,
+                                                              vcpkg::shorten_text(description, desc_length)));
         }
     }
 
@@ -181,9 +187,11 @@ namespace vcpkg::Commands
 
         if (!enable_json)
         {
-            print2("The result may be outdated. Run `git pull` to get the latest results.\n"
-                   "\nIf your port is not listed, please open an issue at and/or consider making a pull request:\n"
-                   "    https://github.com/Microsoft/vcpkg/issues\n");
+            msg::println(msg::format(msgSuggestGitPull)
+                             .append_raw("\n")
+                             .append(msgMissingPortSuggestPullRequest)
+                             .append_indent()
+                             .append_raw("-  https://github.com/Microsoft/vcpkg/issues"));
         }
 
         Checks::exit_success(VCPKG_LINE_INFO);
@@ -217,12 +225,12 @@ namespace vcpkg::Commands
         {
             if (full_description)
             {
-                print2(Color::warning, "--%s has no effect on find artifact\n", OPTION_FULLDESC);
+                msg::println_warning(msgArtifactsOptionIncompatibility, msg::option = OPTION_FULLDESC);
             }
 
             if (enable_json)
             {
-                print2(Color::warning, "--x-json has no effect on find artifact\n");
+                msg::println_warning(msgArtifactsOptionIncompatibility, msg::option = "x-json");
             }
 
             Optional<std::string> filter_hash = filter.map(Hash::get_string_sha256);
@@ -254,6 +262,6 @@ namespace vcpkg::Commands
             perform_find_port_and_exit(paths, full_description, enable_json, filter, args.overlay_ports);
         }
 
-        Checks::exit_with_message(VCPKG_LINE_INFO, "The first parmaeter to add must be 'artifact' or 'port'.\n");
+        Checks::msg_exit_with_error(VCPKG_LINE_INFO, msgAddCommandFirstArg);
     }
 }

commit 3231058ff965d6d10875a473d36d3a6358cfc83e

Fix fmt format strings. (#664)
* Fix fmt format strings. Resolves  https://github.com/microsoft/vcpkg/issues/26415

This regressed in https://github.com/microsoft/vcpkg-tool/commit/107754ff90120f28417a30e86e56048e4884fd44

* More!

src/vcpkg/base/messages.cpp
@@ -202,10 +202,9 @@ namespace vcpkg::msg
                             Color::error,
                             fmt::format("INTERNAL ERROR: localization message '{}' has been declared multiple times\n",
                                         msg::detail::get_message_name(i)));
-                        write_unlocalized_text_to_stdout(Color::error, fmt::format("INTERNAL ERROR: first message:\n"));
+                        write_unlocalized_text_to_stdout(Color::error, "INTERNAL ERROR: first message:\n");
                         write_unlocalized_text_to_stdout(Color::none, m.default_strings[i]);
-                        write_unlocalized_text_to_stdout(Color::error,
-                                                         fmt::format("\nINTERNAL ERROR: second message:\n"));
+                        write_unlocalized_text_to_stdout(Color::error, "\nINTERNAL ERROR: second message:\n");
                         write_unlocalized_text_to_stdout(Color::none, m.default_strings[j]);
                         write_unlocalized_text_to_stdout(Color::none, "\n");
                         ::abort();

src/vcpkg/commands.civerifyversions.cpp
@@ -313,13 +313,13 @@ namespace vcpkg::Commands::CIVerifyVersions
             auto port_name = port_path.stem();
             if (Util::Sets::contains(exclusion_set, port_name.to_string()))
             {
-                if (verbose) msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("SKIP: %s\n", port_name));
+                if (verbose) msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("SKIP: {}\n", port_name));
                 continue;
             }
             auto git_tree_it = port_git_tree_map.find(port_name);
             if (git_tree_it == port_git_tree_map.end())
             {
-                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: {}\n", port_name));
                 errors.emplace(Strings::format("Error: While validating port %s.\n"
                                                "       Missing Git SHA.\n"
                                                "       Run:\n\n"
@@ -344,7 +344,7 @@ namespace vcpkg::Commands::CIVerifyVersions
 
             if (manifest_exists && control_exists)
             {
-                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: {}\n", port_name));
                 errors.emplace(
                     Strings::format("Error: While validating port %s.\n"
                                     "       Both a manifest file and a CONTROL file exist in port directory: %s",
@@ -355,7 +355,7 @@ namespace vcpkg::Commands::CIVerifyVersions
 
             if (!manifest_exists && !control_exists)
             {
-                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: {}\n", port_name));
                 errors.emplace(Strings::format("Error: While validating port %s.\n"
                                                "       No manifest file or CONTROL file exist in port directory: %s",
                                                port_name,
@@ -367,7 +367,7 @@ namespace vcpkg::Commands::CIVerifyVersions
             auto versions_file_path = paths.builtin_registry_versions / prefix / Strings::concat(port_name, ".json");
             if (!fs.exists(versions_file_path, IgnoreErrors{}))
             {
-                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: {}\n", port_name));
                 errors.emplace(Strings::format("Error: While validating port %s.\n"
                                                "       Missing expected versions file at: %s\n"
                                                "       Run:\n\n"
@@ -384,14 +384,12 @@ namespace vcpkg::Commands::CIVerifyVersions
 
             if (!maybe_ok)
             {
-                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: %s\n", port_name));
+                msg::write_unlocalized_text_to_stdout(Color::error, fmt::format("FAIL: {}\n", port_name));
                 errors.emplace(maybe_ok.error());
                 continue;
             }
 
-            if (verbose)
-                msg::write_unlocalized_text_to_stdout(Color::none,
-                                                      fmt::format("%s", maybe_ok.value_or_exit(VCPKG_LINE_INFO)));
+            if (verbose) msg::write_unlocalized_text_to_stdout(Color::none, maybe_ok.value_or_exit(VCPKG_LINE_INFO));
         }
 
         if (!errors.empty())

src/vcpkg/commands.find.cpp
@@ -43,7 +43,7 @@ namespace
         if (full_desc)
         {
             msg::write_unlocalized_text_to_stdout(Color::none,
-                                                  fmt::format("%-20s %-16s %s\n",
+                                                  fmt::format("{:20} {:16} {}\n",
                                                               source_paragraph.name,
                                                               full_version,
                                                               Strings::join("\n    ", source_paragraph.description)));
@@ -62,7 +62,7 @@ namespace
             size_t description_size = used_columns < (119 - 40) ? 119 - used_columns : 40;
 
             msg::write_unlocalized_text_to_stdout(Color::none,
-                                                  fmt::format("%-*s %-*s %s\n",
+                                                  fmt::format("{1:{0}} {3:{2}} {4}\n",
                                                               name_columns,
                                                               source_paragraph.name,
                                                               ver_size,
@@ -78,7 +78,7 @@ namespace
         {
             msg::write_unlocalized_text_to_stdout(
                 Color::none,
-                fmt::format("%-37s %s\n", full_feature_name, Strings::join("\n   ", feature_paragraph.description)));
+                fmt::format("{:37} {}\n", full_feature_name, Strings::join("\n   ", feature_paragraph.description)));
         }
         else
         {
@@ -90,7 +90,7 @@ namespace
             size_t desc_length =
                 119 - std::min<size_t>(60, 1 + std::max<size_t>(s_name_and_ver_columns, full_feature_name.size()));
             msg::write_unlocalized_text_to_stdout(Color::none,
-                                                  fmt::format("%-*s %s\n",
+                                                  fmt::format("{1:{0}} {2}\n",
                                                               s_name_and_ver_columns,
                                                               full_feature_name,
                                                               vcpkg::shorten_text(description, desc_length)));