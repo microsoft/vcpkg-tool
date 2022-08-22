62753143b0d9289ed32979040a4cc4ca8c228e3a
Billy Robert O'Neal III <bion@microsoft.com>
2022-08-22 01:25:06 -0700

    Un-double-negate WindowsOnlyCommand

--- include/vcpkg/base/messages.h
+++ include/vcpkg/base/messages.h
@@ -1554,5 +1554,5 @@ namespace vcpkg
                     "The message named {value} starts with warning:, it must be changed to prepend "
                     "WarningMessage in code instead.");
     DECLARE_MESSAGE(WarningsTreatedAsErrors, (), "", "previous warnings being interpreted as errors");
-    DECLARE_MESSAGE(WindowsOnlyCommand, (), "", "This command is not supported on non-Windows platforms.");
+    DECLARE_MESSAGE(WindowsOnlyCommand, (), "", "This command only supports Windows.");
 }
