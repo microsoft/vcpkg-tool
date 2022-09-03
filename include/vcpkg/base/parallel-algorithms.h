74c9e51f6b5ab25a42e5aca90f6ba0adf72b43fd
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-09-03 17:13:21 +0000

    Reduce macros

--- include/vcpkg/base/parallel-algorithms.h
+++ include/vcpkg/base/parallel-algorithms.h
@@ -11,17 +11,15 @@
 
 
 #if defined(USE_PARALLEL_ALG) && !defined(GNU_USE_PARALLEL_ALG)
-#define vcpkg_par_unseq_for_each std::for_each(std::execution::par_unseq,
-#elif defined(GNU_USE_PARALLEL_ALG)
-#define vcpkg_par_unseq_for_each __gnu_parallel::for_each(
-#else
-#define vcpkg_par_unseq_for_each std::for_each(
-#endif
-
-#if defined(USE_PARALLEL_ALG) && !defined(GNU_USE_PARALLEL_ALG)
 #define vcpkg_parallel_for_each std::for_each(std::execution::par,
 #elif defined(GNU_USE_PARALLEL_ALG)
 #define vcpkg_parallel_for_each __gnu_parallel::for_each(
 #else
 #define vcpkg_parallel_for_each std::for_each(
+#endif
+
+#if defined(USE_PARALLEL_ALG) && !defined(GNU_USE_PARALLEL_ALG)
+#define vcpkg_par_unseq_for_each std::for_each(std::execution::par_unseq,
+#else
+#define vcpkg_par_unseq_for_each vcpkg_parallel_for_each
 #endif
