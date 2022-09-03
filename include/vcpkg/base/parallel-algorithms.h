adac45580f81d99429b902df08c829dc72f36e4e
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-09-03 09:39:56 +0000

    Not everyone has tbb

--- include/vcpkg/base/parallel-algorithms.h
+++ include/vcpkg/base/parallel-algorithms.h
@@ -2,7 +2,7 @@
 
 #include <algorithm>
 
-#if !defined(__clang__)
+#if defined(USE_PARALLEL_ALG)
 #include <execution>
 #endif
 
@@ -11,10 +11,10 @@ namespace vcpkg
     template<typename Iter, typename F>
     void parallel_for_each(Iter begin, Iter end, F cb)
     {
-#if defined(__clang__)
-        std::for_each(begin, end, cb);
-#else
+#if defined(USE_PARALLEL_ALG)
         std::for_each(std::execution::par, begin, end, cb);
+#else
+        std::for_each(begin, end, cb);
 #endif
     }
 }
