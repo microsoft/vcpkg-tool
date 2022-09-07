c15a4a56c9332a691a8ff31bc0453dd303ea1577
Victor Romero <viromer@microsoft.com>
2022-09-07 15:36:40 -0700

    simplify tests

--- src/vcpkg-test/metrics.cpp
+++ src/vcpkg-test/metrics.cpp
@@ -7,19 +7,22 @@
 using namespace vcpkg;
 
 template<typename T>
-void validate_enum_values_and_names(View<MetricEntry<T>> entries)
+void validate_enum_values_and_names(View<MetricEntry<T>> entries, const size_t expected_size)
 {
+    REQUIRE(expected_size == entries.size());
+
     size_t enum_value = 0;
     std::set<StringView> used_names;
     for (auto&& m : entries)
     {
         // fails when a metric is not in the right order in the entries array
-        // - check that there are no duplicate metric entries
+        // - check that there are no duplicate or skipped metric entries
         // - check that the order in Metrics::get_<T>_metrics() and in the <T>Metric enum is the same
         REQUIRE(static_cast<size_t>(m.metric) == enum_value);
         ++enum_value;
 
         // fails when there's a repeated metric name
+        REQUIRE(!m.name.empty());
         auto it_names = used_names.find(m.name);
         REQUIRE(it_names == used_names.end());
         used_names.insert(m.name);
@@ -30,22 +33,16 @@ TEST_CASE ("Check metric enum types", "[metrics]")
 {
     SECTION ("define metrics")
     {
-        auto define_metrics = Metrics::get_define_metrics();
-        REQUIRE(define_metrics.size() == static_cast<size_t>(DefineMetric::COUNT));
-        validate_enum_values_and_names(define_metrics);
+        validate_enum_values_and_names(Metrics::get_define_metrics(), static_cast<size_t>(DefineMetric::COUNT));
     }
 
     SECTION ("string metrics")
     {
-        auto string_metrics = Metrics::get_string_metrics();
-        REQUIRE(string_metrics.size() == static_cast<size_t>(StringMetric::COUNT));
-        validate_enum_values_and_names(string_metrics);
+        validate_enum_values_and_names(Metrics::get_string_metrics(), static_cast<size_t>(StringMetric::COUNT));
     }
 
     SECTION ("bool metrics")
     {
-        auto bool_metrics = Metrics::get_bool_metrics();
-        REQUIRE(bool_metrics.size() == static_cast<size_t>(BoolMetric::COUNT));
-        validate_enum_values_and_names(bool_metrics);
+        validate_enum_values_and_names(Metrics::get_bool_metrics(), static_cast<size_t>(BoolMetric::COUNT));
     }
 }
