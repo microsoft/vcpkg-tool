d8cf0580070c7e5596bddaf70b1e7f1565ccbcdb
Billy O'Neal <bion@microsoft.com>
2022-09-20 17:58:38 -0700

    Implement the --tags command line switch.

--- ce/ce/cli/commands/acquire-project.ts
+++ ce/ce/cli/commands/acquire-project.ts
@@ -1,12 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { buildRegistryResolver, resolveDependencies } from '../../artifacts/artifact';
+import { buildRegistryResolver, ResolvedArtifact, resolveDependencies } from '../../artifacts/artifact';
 import { i } from '../../i18n';
 import { session } from '../../main';
 import { acquireArtifacts, showArtifacts } from '../artifacts';
 import { Command } from '../command';
 import { error } from '../styling';
+import { AcquisitionTags } from '../switches/acquisition-tags';
 import { Project } from '../switches/project';
 
 export class AcquireProjectCommand extends Command {
@@ -15,6 +16,7 @@ export class AcquireProjectCommand extends Command {
   seeAlso = [];
   argumentsHelp = [];
   project: Project = new Project(this);
+  acquisitionTags: AcquisitionTags = new AcquisitionTags(this);
 
   get summary() {
     return i`Acquires everything referenced by a project, without activating`;
@@ -35,14 +37,29 @@ export class AcquireProjectCommand extends Command {
 
     const projectResolver = await buildRegistryResolver(session, projectManifest.metadata.registries);
     const resolved = await resolveDependencies(session, projectResolver, [projectManifest], 3);
+    let toAcquire = resolved;
+    const activeTagsInput = this.acquisitionTags.value;
+    if (activeTagsInput !== undefined) {
+      toAcquire = new Array<ResolvedArtifact>();
+      const activeTags = new Set<string>(activeTagsInput.split(','));
+      activeTags.delete('');
+      for (const artifact of resolved) {
+        for (const tagInArtifact of artifact.artifact.metadata.acquisitionTags) {
+          if (activeTags.has(tagInArtifact)) {
+            toAcquire.push(artifact);
+            break;
+          }
+        }
+      }
+    }
 
     // print the status of what is going to be acquired
-    if (!await showArtifacts(resolved, projectResolver, {force: this.commandLine.force})) {
+    if (!await showArtifacts(toAcquire, projectResolver, {force: this.commandLine.force})) {
       session.channels.error(i`Unable to acquire project`);
       return false;
     }
 
-    return await acquireArtifacts(session, resolved, projectResolver, {
+    return await acquireArtifacts(session, toAcquire, projectResolver, {
       force: this.commandLine.force,
       allLanguages: this.commandLine.allLanguages,
       language: this.commandLine.language
