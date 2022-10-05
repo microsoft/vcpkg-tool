957e711ef6130dab35b02d05350f16dbb2be37f0
Billy O'Neal <bion@microsoft.com>
2022-10-04 21:24:34 -0700

    Fix imprecise env-passthrough test.

--- azure-pipelines/end-to-end-tests-dir/env-passthrough.ps1
+++ azure-pipelines/end-to-end-tests-dir/env-passthrough.ps1
@@ -5,13 +5,13 @@ if (-not $IsLinux -and -not $IsMacOS) {
     $env:_VCPKG_TEST_UNTRACKED = "b"
 
     $x = Run-Vcpkg "--overlay-triplets=$PSScriptRoot/../e2e_ports/env-passthrough" env "echo %_VCPKG_TEST_TRACKED% %_VCPKG_TEST_TRACKED2% %_VCPKG_TEST_UNTRACKED% %_VCPKG_TEST_UNTRACKED2%"
-    if ($x -ne "%_VCPKG_TEST_TRACKED% %_VCPKG_TEST_TRACKED2% %_VCPKG_TEST_UNTRACKED% %_VCPKG_TEST_UNTRACKED2%")
+    if ($x -ne "%_VCPKG_TEST_TRACKED% %_VCPKG_TEST_TRACKED2% %_VCPKG_TEST_UNTRACKED% %_VCPKG_TEST_UNTRACKED2%`r`n")
     {
         throw "env should have cleaned the environment ($x)"
     }
 
     $y = Run-Vcpkg "--overlay-triplets=$PSScriptRoot/../e2e_ports/env-passthrough" env --triplet passthrough "echo %_VCPKG_TEST_TRACKED% %_VCPKG_TEST_TRACKED2% %_VCPKG_TEST_UNTRACKED% %_VCPKG_TEST_UNTRACKED2%"
-    if ($y -ne "a %_VCPKG_TEST_TRACKED2% b %_VCPKG_TEST_UNTRACKED2%")
+    if ($y -ne "a %_VCPKG_TEST_TRACKED2% b %_VCPKG_TEST_UNTRACKED2%`r`n")
     {
         throw "env should have kept the environment ($y)"
     }
