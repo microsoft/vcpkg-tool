649fc7ee8bed00e1b26af2846f610a74d961dd5a
Billy O'Neal <bion@microsoft.com>
2022-10-05 15:23:25 -0700

    Implement cmd_execute_background for non-Windows platforms in terms of posix_spawn.

--- src/vcpkg/base/system.process.cpp
+++ src/vcpkg/base/system.process.cpp
@@ -22,6 +22,8 @@
 
 #if defined(_WIN32)
 #pragma comment(lib, "Advapi32")
+#else
+#include <spawn.h>
 #endif
 
 namespace
@@ -41,7 +43,7 @@ namespace
                                  msg::exit_code = error_value,
                                  msg::error_msg = std::system_category().message(static_cast<int>(error_value)));
     }
-}
+} // unnamed namespace
 
 namespace vcpkg
 {
@@ -719,22 +721,6 @@ namespace vcpkg
 #endif
 
 #if defined(_WIN32)
-    void cmd_execute_background(const Command& cmd_line)
-    {
-        const ElapsedTimer timer;
-        auto process_info =
-            windows_create_windowless_process(cmd_line.command_line(),
-                                              default_working_directory,
-                                              default_environment,
-                                              CREATE_NEW_CONSOLE | CREATE_NO_WINDOW | CREATE_BREAKAWAY_FROM_JOB);
-        if (!process_info)
-        {
-            Debug::print("cmd_execute_background() failed: ", process_info.error(), "\n");
-        }
-
-        Debug::print("cmd_execute_background() took ", static_cast<int>(timer.microseconds()), " us\n");
-    }
-
     Environment cmd_execute_and_capture_environment(const Command& cmd_line, const Environment& env)
     {
         static StringLiteral magic_string = "cdARN4xjKueKScMy9C6H";
@@ -785,6 +771,46 @@ namespace vcpkg
         return new_env;
     }
 #endif
+
+    void cmd_execute_background(const Command& cmd_line)
+    {
+        Debug::println("cmd_execute_background: ", cmd_line.command_line());
+#if defined(_WIN32)
+        auto process_info =
+            windows_create_windowless_process(cmd_line.command_line(),
+                                              default_working_directory,
+                                              default_environment,
+                                              CREATE_NEW_CONSOLE | CREATE_NO_WINDOW | CREATE_BREAKAWAY_FROM_JOB);
+        if (!process_info)
+        {
+            Debug::println("cmd_execute_background() failed: ", process_info.error());
+        }
+#else  // ^^^ _WIN32 // !_WIN32
+        pid_t pid;
+
+        std::vector<std::string> argv_builder; // as if by system()
+        argv_builder.reserve(3);
+        argv_builder.emplace_back("sh");
+        argv_builder.emplace_back("-c");
+        StringView command_line = cmd_line.command_line();
+        argv_builder.emplace_back(command_line.data(), command_line.size());
+
+        std::vector<char*> argv;
+        argv.reserve(argv_builder.size() + 1);
+        for (std::string& arg : argv_builder)
+        {
+            argv.emplace_back(arg.data());
+        }
+
+        argv.emplace_back(nullptr);
+
+        int error = posix_spawn(&pid, "/bin/sh", nullptr /*file_actions*/, nullptr /*attrp*/, argv.data(), environ);
+        if (error)
+        {
+            Debug::println(fmt::format("cmd_execute_background() failed: {}", error));
+        }
+#endif // ^^^ !_WIN32
+    }
 
     static ExpectedL<int> cmd_execute_impl(const Command& cmd_line, const WorkingDirectory& wd, const Environment& env)
     {
--- src/vcpkg/metrics.cpp
+++ src/vcpkg/metrics.cpp
@@ -566,54 +566,39 @@ namespace vcpkg
             return;
         }
 
-#if defined(_WIN32)
-        wchar_t temp_folder[MAX_PATH];
-        GetTempPathW(MAX_PATH, temp_folder);
-
-        const Path temp_folder_path = Path(Strings::to_utf8(temp_folder)) / "vcpkg";
-        const Path temp_folder_path_exe = temp_folder_path / "vcpkg-" VCPKG_BASE_VERSION_AS_STRING ".exe";
-#endif
-
-        std::error_code ec;
-#if defined(_WIN32)
-        fs.create_directories(temp_folder_path, ec);
-        if (ec) return;
-        fs.copy_file(get_exe_path_of_current_process(), temp_folder_path_exe, CopyOptions::skip_existing, ec);
-        if (ec) return;
-#else
-        if (!fs.exists("/tmp", IgnoreErrors{})) return;
-        const Path temp_folder_path = "/tmp/vcpkg";
-        fs.create_directory(temp_folder_path, IgnoreErrors{});
-#endif
+        const Path temp_folder_path = fs.create_or_get_temp_directory(VCPKG_LINE_INFO);
         const Path vcpkg_metrics_txt_path = temp_folder_path / ("vcpkg" + generate_random_UUID() + ".txt");
+        Debug::println("Uploading metrics ", vcpkg_metrics_txt_path);
+        std::error_code ec;
         fs.write_contents(vcpkg_metrics_txt_path, payload, ec);
         if (ec) return;
 
 #if defined(_WIN32)
+        const Path temp_folder_path_exe = temp_folder_path / "vcpkg-" VCPKG_BASE_VERSION_AS_STRING ".exe";
+        fs.copy_file(get_exe_path_of_current_process(), temp_folder_path_exe, CopyOptions::skip_existing, ec);
+        if (ec) return;
         Command builder;
         builder.string_arg(temp_folder_path_exe);
         builder.string_arg("x-upload-metrics");
         builder.string_arg(vcpkg_metrics_txt_path);
         cmd_execute_background(builder);
 #else
-        // TODO: convert to cmd_execute_background or something.
-        auto curl = Command("curl")
-                        .string_arg("https://dc.services.visualstudio.com/v2/track")
-                        .string_arg("--max-time")
-                        .string_arg("3")
-                        .string_arg("-H")
-                        .string_arg("Content-Type: application/json")
-                        .string_arg("-X")
-                        .string_arg("POST")
-                        .string_arg("--tlsv1.2")
-                        .string_arg("--data")
-                        .string_arg(Strings::concat("@", vcpkg_metrics_txt_path))
-                        .raw_arg(">/dev/null")
-                        .raw_arg("2>&1");
-        auto remove = Command("rm").string_arg(vcpkg_metrics_txt_path);
-        Command cmd_line;
-        cmd_line.raw_arg("(").raw_arg(curl.command_line()).raw_arg(";").raw_arg(remove.command_line()).raw_arg(") &");
-        cmd_execute_clean(cmd_line);
+        cmd_execute_background(Command("curl")
+                                   .string_arg("https://dc.services.visualstudio.com/v2/track")
+                                   .string_arg("--max-time")
+                                   .string_arg("60")
+                                   .string_arg("-H")
+                                   .string_arg("Content-Type: application/json")
+                                   .string_arg("-X")
+                                   .string_arg("POST")
+                                   .string_arg("--tlsv1.2")
+                                   .string_arg("--data")
+                                   .string_arg(Strings::concat("@", vcpkg_metrics_txt_path))
+                                   .raw_arg(">/dev/null")
+                                   .raw_arg("2>&1")
+                                   .raw_arg(";")
+                                   .string_arg("rm")
+                                   .string_arg(vcpkg_metrics_txt_path));
 #endif
     }
 }
