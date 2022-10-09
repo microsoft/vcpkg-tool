e6c58a6db880a170c379d055428421ac32445366
Victor Romero <viromer@microsoft.com>
2022-10-08 19:25:01 -0700

    [telemetry] Track help topic

--- src/vcpkg/help.cpp
+++ src/vcpkg/help.cpp
@@ -13,6 +13,7 @@
 #include <vcpkg/export.h>
 #include <vcpkg/help.h>
 #include <vcpkg/install.h>
+#include <vcpkg/metrics.h>
 #include <vcpkg/remove.h>
 #include <vcpkg/vcpkgpaths.h>
 
@@ -190,6 +191,7 @@ namespace vcpkg::Help
         if (topic == "triplets" || topic == "triple")
         {
             help_topic_valid_triplet(paths);
+            LockGuardPtr<Metrics>(g_metrics)->track_string_property(StringMetric::CommandContext, topic);
             Checks::exit_success(VCPKG_LINE_INFO);
         }
 
@@ -197,11 +199,13 @@ namespace vcpkg::Help
         if (it_topic != topics.end())
         {
             it_topic->print(paths);
+            LockGuardPtr<Metrics>(g_metrics)->track_string_property(StringMetric::CommandContext, it_topic->name);
             Checks::exit_success(VCPKG_LINE_INFO);
         }
 
         print2(Color::error, "Error: unknown topic ", topic, '\n');
         help_topics(paths);
+        LockGuardPtr<Metrics>(g_metrics)->track_string_property(StringMetric::CommandContext, "unknown");
         Checks::exit_fail(VCPKG_LINE_INFO);
     }
 
