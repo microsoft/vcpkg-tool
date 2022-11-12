9888a20c5fcd05b2e4d4f06b12ff7df104a73c0b
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-11-12 23:29:10 +0100

    remove ps assignment

--- azure-pipelines/end-to-end-tests-dir/hash-additional.ps1
+++ azure-pipelines/end-to-end-tests-dir/hash-additional.ps1
@@ -1,3 +1,3 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
-$y = Run-Vcpkg "--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional" "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional" install port-hash-additional --triplet hash-additional-e2e 
+Run-Vcpkg "--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional" "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional" install port-hash-additional --triplet hash-additional-e2e 
 Throw-IfFailed
