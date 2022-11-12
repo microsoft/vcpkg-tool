8f97bbf13ddf064d3850d1084963d3e9d8097d09
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-11-12 23:28:41 +0100

    use TARGET_TRIPLET var

--- azure-pipelines/e2e_ports/hash-additional/port/portfile.cmake
+++ azure-pipelines/e2e_ports/hash-additional/port/portfile.cmake
@@ -1,4 +1,4 @@
-file(STRINGS "${CURRENT_BUILDTREES_DIR}/hash-additional-e2e.vcpkg_abi_info.txt" lines)
+file(STRINGS "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}.vcpkg_abi_info.txt" lines)
 list(GET lines 0 first_line)
 
 set(expected "additional_abi_file.txt 61ba0c7fc1f696e28c1b7aa9460980a571025ff8c97bb90a57e990463aa25660")
