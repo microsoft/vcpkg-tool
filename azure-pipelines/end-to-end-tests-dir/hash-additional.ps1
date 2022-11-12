867f2764318da6d0422829a6ddcedd21654fe226
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-11-12 23:02:55 +0100

    Fail CI if unsuccesful

--- azure-pipelines/end-to-end-tests-dir/hash-additional.ps1
+++ azure-pipelines/end-to-end-tests-dir/hash-additional.ps1
@@ -1,2 +1,3 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
 $y = Run-Vcpkg "--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional" "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional" install port-hash-additional --triplet hash-additional-e2e 
+Throw-IfFailed
