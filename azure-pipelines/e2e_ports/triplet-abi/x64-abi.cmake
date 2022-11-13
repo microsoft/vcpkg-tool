5aeafa6bb24f3aedcafec3eeb1b9404e2ba920a1
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-11-13 10:28:06 +0100

    fix osx triplet

--- azure-pipelines/e2e_ports/triplet-abi/x64-abi.cmake
+++ azure-pipelines/e2e_ports/triplet-abi/x64-abi.cmake
@@ -3,7 +3,7 @@ set(VCPKG_CRT_LINKAGE dynamic) #
 set(VCPKG_LIBRARY_LINKAGE static) #
 if(0)
 set(VCPKG_CMAKE_SYSTEM_NAME Windows) # NOT IN ABI COMMENT
-elseif(DARWIN)
+elseif(APPLE)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 elseif(UNIX)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
