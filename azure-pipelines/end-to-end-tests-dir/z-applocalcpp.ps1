374d4ba380422bccbc7963ba07a30af03881a375
Valeria Conde <valeriaconde.96@gmail.com>
2022-12-08 11:15:35 -0800

    please work

--- azure-pipelines/end-to-end-tests-dir/z-applocalcpp.ps1
+++ azure-pipelines/end-to-end-tests-dir/z-applocalcpp.ps1
@@ -1,15 +1,14 @@
-. $PSScriptRoot/../end-to-end-tests-prelude.ps1
-
-# Path to my simple project
-$buildDir = "$PSScriptRoot/../e2e_projects/applocal-test/build"
-
 if ($IsWindows) {
+    . $PSScriptRoot/../end-to-end-tests-prelude.ps1
+
+    # Path to my simple project
+    $buildDir = "$PSScriptRoot/../e2e_projects/applocal-test/build"
+
     # Tests z-applocal command no logs
     Run-Vcpkg z-applocal `
             --target-binary=$buildDir/Debug/main.exe `
             --installed-bin-dir=$buildDir/x64-windows/debug/bin
     Throw-IfFailed
-
 
     # Tests z-applocal command with logs
     Run-Vcpkg z-applocal `
@@ -23,12 +22,10 @@ if ($IsWindows) {
     Run-Vcpkg z-applocal
     Throw-IfNotFailed
 
-
     # Tests z-applocal with no installed-bin-dir argument
     Run-Vcpkg z-applocal `
             --target-binary=$buildDir/Debug/main.exe
     Throw-IfNotFailed
-
 
     # Tests z-applocal with no target-binary argument
     Run-Vcpkg z-applocal `
