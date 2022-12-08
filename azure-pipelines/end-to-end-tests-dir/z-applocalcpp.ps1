05e67c92e68c638be148179dc4e4a93ee72d3b64
Valeria Conde <valeriaconde.96@gmail.com>
2022-12-08 11:05:23 -0800

    added architecture check on e2e tests

--- azure-pipelines/end-to-end-tests-dir/z-applocalcpp.ps1
+++ azure-pipelines/end-to-end-tests-dir/z-applocalcpp.ps1
@@ -1,32 +1,37 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
 
-# Tests a simple project 
+# Path to my simple project
 $buildDir = "$PSScriptRoot/../e2e_projects/applocal-test/build"
 
-# Tests z-applocal command no logs
-Run-Vcpkg z-applocal `
-        --target-binary=$buildDir/Debug/main.exe `
-        --installed-bin-dir=$buildDir/x64-windows/debug/bin
-Throw-IfFailed
+if ($IsWindows) {
+    # Tests z-applocal command no logs
+    Run-Vcpkg z-applocal `
+            --target-binary=$buildDir/Debug/main.exe `
+            --installed-bin-dir=$buildDir/x64-windows/debug/bin
+    Throw-IfFailed
 
-# Tests z-applocal command with logs
-Run-Vcpkg z-applocal `
-        --target-binary=$buildDir/Debug/main.exe `
-        --installed-bin-dir=$buildDir/x64-windows/debug/bin `
-        --tlog-file=$buildDir/logs/vcpkg.applocal.tlog `
-        --copied-files-log=$buildDir/logs/vcpkg.applocal.log
-Throw-IfFailed
 
-# Tests z-applocal command with no arguments
-Run-Vcpkg z-applocal
-Throw-IfNotFailed
+    # Tests z-applocal command with logs
+    Run-Vcpkg z-applocal `
+            --target-binary=$buildDir/Debug/main.exe `
+            --installed-bin-dir=$buildDir/x64-windows/debug/bin `
+            --tlog-file=$buildDir/logs/vcpkg.applocal.tlog `
+            --copied-files-log=$buildDir/logs/vcpkg.applocal.log
+    Throw-IfFailed
 
-# Tests z-applocal with no installed-bin-dir argument
-Run-Vcpkg z-applocal `
-        --target-binary=$buildDir/Debug/main.exe
-Throw-IfNotFailed
+    # Tests z-applocal command with no arguments
+    Run-Vcpkg z-applocal
+    Throw-IfNotFailed
 
-# Tests z-applocal with no target-binary argument
-Run-Vcpkg z-applocal `
-        --installed-bin-dir=$buildDir/x64-windows/debug/bin
-Throw-IfNotFailed
+
+    # Tests z-applocal with no installed-bin-dir argument
+    Run-Vcpkg z-applocal `
+            --target-binary=$buildDir/Debug/main.exe
+    Throw-IfNotFailed
+
+
+    # Tests z-applocal with no target-binary argument
+    Run-Vcpkg z-applocal `
+            --installed-bin-dir=$buildDir/x64-windows/debug/bin
+    Throw-IfNotFailed
+}
