0d90fa89b6f6bdc65fa4f16228b104b8fae5b315
Victor Romero <romerosanchezv@gmail.com>
2022-12-10 03:23:16 +0000

    Fix e2e tests

--- azure-pipelines/end-to-end-tests-dir/export-port.ps1
+++ azure-pipelines/end-to-end-tests-dir/export-port.ps1
@@ -5,7 +5,7 @@ $testPorts = "$PSScriptRoot/../e2e_ports/version-files/ports"
 $testVersionsDb = "$PSScriptRoot/../e2e_ports/version-files/versions"
 
 # [x-export-port] Export local port files
-Run-Vcpkg -EndToEndTestSilent x-export-port cat "$testOutput" `
+Run-Vcpkg -EndToEndTestSilent x-export-port cat "$testOutput/cat" `
     --x-builtin-ports-root="$testPorts" `
     | Out-Null
 Throw-IfFailed
@@ -19,7 +19,7 @@ if ($diff) {
 }
 
 # [x-export-port] Fail local port file not exists
-Run-Vcpkg -EndToEndTestSilent x-export-port unicorn "${testOutput}" `
+Run-Vcpkg -EndToEndTestSilent x-export-port unicorn "${testOutput}/unicorn" `
     --x-builtin-ports-root="$testPorts" `
     | Out-Null
 Throw-IfNotFailed
@@ -28,7 +28,7 @@ Throw-IfNotFailed
 Refresh-TestRoot
 New-Item -ItemType "directory" -Path "$testOutput/dog" | Out-Null
 Out-File -FilePath "$testOutput/dog/HelloWorld.txt" | Out-Null
-Run-Vcpkg -EndToEndTestSilent x-export-port dog "$testOutput" `
+Run-Vcpkg -EndToEndTestSilent x-export-port dog "$testOutput/dog" `
     --x-builtin-ports-root="$testPorts" `
     | Out-Null
 Throw-IfNotFailed -Message "[x-export-port] Refuse to export to non-empty directory"
@@ -37,7 +37,7 @@ Throw-IfNotFailed -Message "[x-export-port] Refuse to export to non-empty direct
 Refresh-TestRoot
 New-Item -ItemType "directory" -Path "$testOutput/dog" | Out-Null
 Out-File -FilePath "$testOutput/dog/HelloWorld.txt" | Out-Null
-Run-Vcpkg -EndToEndTestSilent x-export-port dog "$testOutput" --force `
+Run-Vcpkg -EndToEndTestSilent x-export-port dog "$testOutput/dog" --force `
     --x-builtin-ports-root="$testPorts" `
     | Out-Null
 Throw-IfFailed
@@ -52,11 +52,11 @@ if ($diff) {
 
 # [x-export-port] Don't create package sub-directory
 Refresh-TestRoot
-Run-Vcpkg -EndToEndTestSilent x-export-port mouse "$testOutput/mickey" --no-subdir `
+Run-Vcpkg -EndToEndTestSilent x-export-port mouse "$testOutput" --subdir `
     --x-builtin-ports-root="$testPorts" `
     | Out-Null
 Throw-IfFailed
-$diff = git diff --no-index -- "$testPorts/mouse" "$testOutput/mickey"
+$diff = git diff --no-index -- "$testPorts/mouse" "$testOutput/mouse"
 if ($diff) {
     Refresh-TestRoot
     Write-Trace "error: [x-export-port] Don't create package sub-directory"
@@ -67,7 +67,7 @@ if ($diff) {
 
 # [x-export-port] Export version port files
 Refresh-TestRoot
-Run-Vcpkg -EndToEndTestSilent x-export-port duck --version=mallard "$testOutput" `
+Run-Vcpkg -EndToEndTestSilent x-export-port duck@mallard "$testOutput/duck" `
     --x-builtin-ports-root="$testPorts" `
     --x-builtin-registry-versions-dir="$testVersionsDb" `
     | Out-Null
@@ -81,36 +81,9 @@ if ($diff) {
     throw "exported files don't match the source"
 }
 
-# [x-export-port] Add version suffix to destination
-Refresh-TestRoot
-Run-Vcpkg -EndToEndTestSilent x-export-port duck --version=mallard "$testOutput" --add-version-suffix `
-    --x-builtin-ports-root="$testPorts" `
-    --x-builtin-registry-versions-dir="$testVersionsDb" `
-    | Out-Null
-Throw-IfFailed
-$diff = git diff --no-index -- "$testPorts/duck" "$testOutput/duck-mallard"
-if ($diff) {
-    Refresh-TestRoot
-    Write-Trace "error: [x-export-port] Add version suffix to destination"
-    Write-Host $Script::CurrentTest
-    $diff
-    throw "exported files don't match the source"
-}
-
-# [x-export-port] Fail if no destination and no overlays
-Refresh-TestRoot
-Run-Vcpkg -EndToEndTestSilent x-export-port duck --version=mallard `
+# [x-export-port] Fail if no destination
+Run-Vcpkg -EndToEndTestSilent x-export-port duck@mallard `
     --x-builtin-ports-root="$testPorts" `
     --x-builtin-registry-versions-dir="$testVersionsDb" `
     | Out-Null
 Throw-IfNotFailed
-
-# [x-export-port] Fail if no destination and no overlays
-Refresh-TestRoot
-Run-Vcpkg -EndToEndTestSilent x-export-port duck --version=mallard --overlay-ports="$testOutput" `
-    --x-builtin-ports-root="$testPorts" `
-    --x-builtin-registry-versions-dir="$testVersionsDb" `
-    | Out-Null
-Throw-IfFailed
-
-Refresh-TestRoot
