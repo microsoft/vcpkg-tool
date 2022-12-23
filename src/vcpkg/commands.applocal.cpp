cf69707862c33ea6684bc83c88d3ca4191cc52fd
Valeria Conde <valeriaconde.96@gmail.com>
2022-12-22 16:03:05 -0800

    Changed mutant

--- src/vcpkg/commands.applocal.cpp
+++ src/vcpkg/commands.applocal.cpp
@@ -117,7 +117,7 @@ namespace
                 }
                 else
                 {
-                    Debug::println("  ", imported_name, ": ", installed_item_file_path.c_str(), " not found");
+                    Debug::println("  ", imported_name, ": ", installed_item_file_path, " not found");
                 }
             }
         }
@@ -415,16 +415,33 @@ namespace
             const auto source = installed_dir / target_binary_name;
             const auto target = target_binary_dir / target_binary_name;
 
-            const auto mutant_name = Strings::to_utf16("vcpkg-applocal-" + Hash::get_string_sha256(target_binary_dir));
-
-            HANDLE the_mutant = ::CreateMutexW(nullptr, FALSE, mutant_name.c_str());
-
-            if (the_mutant == NULL)
+            const auto mutant_name = "vcpkg-applocal-" + Hash::get_string_sha256(target_binary_dir);
+            struct MutantGuard
             {
-                Checks::exit_with_message(VCPKG_LINE_INFO, "Failed\n");
+                MutantGuard(StringView name)
+                {
+                    h = ::CreateMutexW(nullptr, FALSE, Strings::to_utf16(name).c_str());
+                    if (h)
+                    {
+                        WaitForSingleObject(h, INFINITE);
+                    }
+                }
+                explicit operator bool() const { return h; }
+                ~MutantGuard()
+                {
+                    if (h)
+                    {
+                        ReleaseMutex(h);
+                        CloseHandle(h);
+                    }
+                }
+                HANDLE h;
+            };
+            const MutantGuard mutant(mutant_name);
+            if (!mutant)
+            {
+                Checks::exit_with_message(VCPKG_LINE_INFO, "Failed to acquire mutant %s", mutant_name);
             }
-
-            WaitForSingleObject(the_mutant, INFINITE);
 
             std::error_code ec;
             const bool did_deploy = m_fs.copy_file(source, target, CopyOptions::update_existing, ec);
@@ -439,9 +456,6 @@ namespace
             else if (ec == std::errc::no_such_file_or_directory)
             {
                 Debug::println("Attempted to deploy ", source, ", but it didn't exist");
-
-                ReleaseMutex(the_mutant);
-                CloseHandle(the_mutant);
 
                 return false;
             }
@@ -468,9 +482,6 @@ namespace
                                    m_copied_files_log.write(native.c_str(), 1, native.size()) == native.size());
                 Checks::check_exit(VCPKG_LINE_INFO, m_copied_files_log.put('\n') == '\n');
             }
-
-            ReleaseMutex(the_mutant);
-            CloseHandle(the_mutant);
 
             return did_deploy;
         }
