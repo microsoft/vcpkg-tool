e73abdc4f6fde98a24fb70a838e253be7623afb0
Valeria Conde <valeriaconde.96@gmail.com>
2022-12-28 19:17:52 -0800

    e2e test

---
+++ azure-pipelines/e2e_projects/applocal-test/plugins/azure_kinect_sensor_sdk/installed_dir/depthengine_2_0.cpp
@@ -0,0 +1,7 @@
+#define MYLIB_EXPORTS 1
+
+#include "depthengine_2_0.h"
+
+void depthengine_2_0::my_func() {
+	puts("hello world");
+}
---
+++ azure-pipelines/e2e_projects/applocal-test/plugins/azure_kinect_sensor_sdk/installed_dir/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "k4a.h"
+
+int main() {
+    k4a::my_func();
+
+	return 0;
+}
