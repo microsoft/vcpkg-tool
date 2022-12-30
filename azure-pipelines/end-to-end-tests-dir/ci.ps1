24d769bd2a7aba1df0fb7e5a1eda20832a0899f0
Kai Pastor <dg0yt@darc.de>
2022-12-30 12:36:31 +0100

    Test feature/supports impact on vcpkg ci

--- azure-pipelines/end-to-end-tests-dir/ci.ps1
+++ azure-pipelines/end-to-end-tests-dir/ci.ps1
@@ -11,3 +11,16 @@ Throw-IfNotFailed
 # Clearly not the correct answer
 Run-Vcpkg ci --triplet=$Triplet --x-skipped-cascade-count=1000
 Throw-IfNotFailed
+
+# Regular ci; may take a few seconds to complete
+$port_list = Run-VcpkgAndCaptureOutput ci --dry-run --triplet=$Triplet --binarysource=clear --overlay-ports="$PSScriptRoot/../e2e_ci"
+Throw-IfFailed
+if ($port_list -match "zzz-unexpected-port[^ ]*[ ]*->") {
+    throw 'Detected installation of "zzz-unexpected-port"'
+}
+if ($port_list -match "zzz-unexpected-feature[^ ]*[ ]*->") {
+    throw 'Detected installation of "zzz-unexpected-feature"'
+}
+if ($port_list -notmatch "zzz-expected-port[^ ]*[ ]*->") {
+    throw 'Did not detect installation of "zzz-expected-port"'
+}
