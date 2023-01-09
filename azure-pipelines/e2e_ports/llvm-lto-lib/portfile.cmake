0801227bb5a53fe69c649ffefb86c6673b7fda58
Billy Robert O'Neal III <bion@microsoft.com>
2023-01-09 15:42:47 -0800

    Whoops dupe directory :)

--- azure-pipelines/e2e_ports/llvm-lto-lib/portfile.cmake
+++ azure-pipelines/e2e_ports/llvm-lto-lib/portfile.cmake
@@ -2,9 +2,9 @@ file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib")
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug")
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/lib")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/llvm-lto-charset.lib"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/lib/llvm-lto-charset.lib")
+    DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/llvm-lto-charset.lib"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/lib/debug/llvm-lto-charset.lib")
+    DESTINATION "${CURRENT_PACKAGES_DIR}/lib/debug")
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include")
 file(TOUCH "${CURRENT_PACKAGES_DIR}/include/llvm-lto-lib.h")
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share")
