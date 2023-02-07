60fae76c832b6d5f1b6525b80248117199796716
Billy Robert O'Neal III <bion@microsoft.com>
2023-02-06 16:27:51 -0800

    Use TestingRoot rather than manually cleaning WorkingRoot.

--- azure-pipelines/end-to-end-tests-dir/post-build-checks.ps1
+++ azure-pipelines/end-to-end-tests-dir/post-build-checks.ps1
@@ -16,16 +16,12 @@ if (-not $buildOutput.Contains("$packagesRoot\vcpkg-internal-dll-with-no-exports
 
 # DLLs with wrong architecture
 Refresh-TestRoot
-if (Test-Path "$WorkingRoot/wrong-architecture") {
-    Remove-Item "$WorkingRoot/wrong-architecture" -Recurse -Force
-}
-
-mkdir "$WorkingRoot/wrong-architecture"
-Copy-Item -Recurse "$PSScriptRoot/../e2e_assets/test-dll-port-template" "$WorkingRoot/wrong-architecture/test-dll"
-Run-Vcpkg env "$WorkingRoot/wrong-architecture/test-dll/build.cmd" --Triplet x64-windows
+mkdir "$TestingRoot/wrong-architecture"
+Copy-Item -Recurse "$PSScriptRoot/../e2e_assets/test-dll-port-template" "$TestingRoot/wrong-architecture/test-dll"
+Run-Vcpkg env "$TestingRoot/wrong-architecture/test-dll/build.cmd" --Triplet x64-windows
 Throw-IfFailed
 
-$buildOutput = Run-VcpkgAndCaptureOutput @commonArgs install --overlay-ports="$WorkingRoot/wrong-architecture" test-dll --no-binarycaching
+$buildOutput = Run-VcpkgAndCaptureOutput @commonArgs install --overlay-ports="$TestingRoot/wrong-architecture" test-dll --no-binarycaching
 $expected = "warning: The following files were built for an incorrect architecture:`n" + `
 "warning:     $packagesRoot\test-dll_x86-windows\debug\lib\test_dll.lib`n" + `
 " Expected: x86, but was x64`n" + `
@@ -43,16 +39,12 @@ if (-not $buildOutput.Replace("`r`n", "`n").Contains($expected)) {
 
 # DLLs with no AppContainer bit
 Refresh-TestRoot
-if (Test-Path "$WorkingRoot/wrong-appcontainer") {
-    Remove-Item "$WorkingRoot/wrong-appcontainer" -Recurse -Force
-}
-
-mkdir "$WorkingRoot/wrong-appcontainer"
-Copy-Item -Recurse "$PSScriptRoot/../e2e_assets/test-dll-port-template" "$WorkingRoot/wrong-appcontainer/test-dll"
-Run-Vcpkg env "$WorkingRoot/wrong-appcontainer/test-dll/build.cmd" --Triplet x64-windows
+mkdir "$TestingRoot/wrong-appcontainer"
+Copy-Item -Recurse "$PSScriptRoot/../e2e_assets/test-dll-port-template" "$TestingRoot/wrong-appcontainer/test-dll"
+Run-Vcpkg env "$TestingRoot/wrong-appcontainer/test-dll/build.cmd" --Triplet x64-windows
 Throw-IfFailed
 
-$buildOutput = Run-VcpkgAndCaptureOutput --triplet x64-uwp "--x-buildtrees-root=$buildtreesRoot" "--x-install-root=$installRoot" "--x-packages-root=$packagesRoot" install --overlay-ports="$WorkingRoot/wrong-appcontainer" test-dll --no-binarycaching
+$buildOutput = Run-VcpkgAndCaptureOutput --triplet x64-uwp "--x-buildtrees-root=$buildtreesRoot" "--x-install-root=$installRoot" "--x-packages-root=$packagesRoot" install --overlay-ports="$TestingRoot/wrong-appcontainer" test-dll --no-binarycaching
 $expected = "warning: The App Container bit must be set for Windows Store apps. The following DLLs do not have the App Container bit set:`n" + `
 "`n" + `
 "    $packagesRoot\test-dll_x64-uwp\debug\bin\test_dll.dll`n" + `
@@ -64,16 +56,12 @@ if (-not $buildOutput.Replace("`r`n", "`n").Contains($expected)) {
 
 # Wrong CRT linkage
 Refresh-TestRoot
-if (Test-Path "$WorkingRoot/wrong-crt") {
-    Remove-Item "$WorkingRoot/wrong-crt" -Recurse -Force
-}
-
-mkdir "$WorkingRoot/wrong-crt"
-Copy-Item -Recurse "$PSScriptRoot/../e2e_assets/test-lib-port-template-dynamic-crt" "$WorkingRoot/wrong-crt/test-lib"
-Run-Vcpkg env "$WorkingRoot/wrong-crt/test-lib/build.cmd" --Triplet x86-windows-static
+mkdir "$TestingRoot/wrong-crt"
+Copy-Item -Recurse "$PSScriptRoot/../e2e_assets/test-lib-port-template-dynamic-crt" "$TestingRoot/wrong-crt/test-lib"
+Run-Vcpkg env "$TestingRoot/wrong-crt/test-lib/build.cmd" --Triplet x86-windows-static
 Throw-IfFailed
 
-$buildOutput = Run-VcpkgAndCaptureOutput --triplet x86-windows-static "--x-buildtrees-root=$buildtreesRoot" "--x-install-root=$installRoot" "--x-packages-root=$packagesRoot" install --overlay-ports="$WorkingRoot/wrong-crt" test-lib --no-binarycaching
+$buildOutput = Run-VcpkgAndCaptureOutput --triplet x86-windows-static "--x-buildtrees-root=$buildtreesRoot" "--x-install-root=$installRoot" "--x-packages-root=$packagesRoot" install --overlay-ports="$TestingRoot/wrong-crt" test-lib --no-binarycaching
 $expected = "warning: The following binaries should use the Static Release CRT.`n" +
 "    $packagesRoot\test-lib_x86-windows-static\debug\lib\both_lib.lib links with:`n" +
 "        Dynamic Debug`n" +
