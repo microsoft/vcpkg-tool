99625ce90f662a790ea8df9d963a2c561436fc55
Billy Robert O'Neal III <bion@microsoft.com>
2023-02-23 20:51:25 -0800

    clang-format

--- include/vcpkg/documentation.h
+++ include/vcpkg/documentation.h
@@ -10,8 +10,7 @@ namespace vcpkg
         static constexpr StringLiteral manifests_url = "https://learn.microsoft.com/vcpkg/users/manifests";
         static constexpr StringLiteral assetcaching_url = "https://learn.microsoft.com/vcpkg/users/assetcaching";
         static constexpr StringLiteral binarycaching_url = "https://learn.microsoft.com/vcpkg/users/binarycaching";
-        static constexpr StringLiteral versioning_url =
-            "https://learn.microsoft.com/vcpkg/users/versioning";
+        static constexpr StringLiteral versioning_url = "https://learn.microsoft.com/vcpkg/users/versioning";
         static constexpr StringLiteral vcpkg_visual_studio_path_url =
             "https://learn.microsoft.com/vcpkg/users/triplets#VCPKG_VISUAL_STUDIO_PATH";
     }
