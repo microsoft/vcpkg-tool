1f7ab755689595dcc33dd56a7fa19f0cc00c633c
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-03-01 20:21:11 +0100

    Fix e2e test after CR

--- azure-pipelines/e2e_ports/hash-additional/vcpkg-test-hash-additional/portfile.cmake
+++ azure-pipelines/e2e_ports/hash-additional/vcpkg-test-hash-additional/portfile.cmake
@@ -1,7 +1,7 @@
 file(STRINGS "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}.vcpkg_abi_info.txt" lines)
 list(GET lines 0 first_line)
 
-set(expected "additional_abi_file.txt 61ba0c7fc1f696e28c1b7aa9460980a571025ff8c97bb90a57e990463aa25660")
+set(expected "additional_file_0 61ba0c7fc1f696e28c1b7aa9460980a571025ff8c97bb90a57e990463aa25660")
 
 if(first_line STREQUAL "${expected}")
     message(STATUS "Test succesful!")
