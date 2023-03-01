538539c4e364dd6f14f5e36f6704144254845159
Billy O'Neal <bion@microsoft.com>
2023-03-01 13:44:54 -0800

    Fix bootstrap wrapper scripts to tolerate deployment within readonly VCPKG_ROOT. (#939)

--- src/vcpkg/configure-environment.cpp
+++ src/vcpkg/configure-environment.cpp
@@ -137,7 +137,7 @@ namespace vcpkg
         download_manager.download_file(fs, ce_uri, {}, ce_tarball, nullopt, null_sink);
         extract_ce_tarball(paths, ce_tarball, node_path, base_path);
 #endif // ^^^ always get latest
-#endif // ^^^ !VCPKG_CE_SHA
+#endif // ^^^ might need to download
 
         auto temp_directory = fs.create_or_get_temp_directory(VCPKG_LINE_INFO);
 
