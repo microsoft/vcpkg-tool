996c60f9f70721d363325ce426f550170629093c
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2023-03-16 11:48:06 +0100

    Fix msvc warning

--- src/vcpkg-test/strings.cpp
+++ src/vcpkg-test/strings.cpp
@@ -38,7 +38,7 @@ TEST_CASE ("b32 encoding", "[strings]")
 TEST_CASE ("percent encoding", "[strings]")
 {
     std::string ascii(127, 0);
-    std::iota(ascii.begin(), ascii.end(), 1);
+    std::iota(ascii.begin(), ascii.end(), static_cast<char>(1));
     REQUIRE(vcpkg::Strings::percent_encode(ascii) ==
             "%01%02%03%04%05%06%07%08%09%0A%0B%0C%0D%0E%0F%10%11%12%13%14%15%16%17%18%19%1A%1B%1C%1D%1E%1F%20%21%22%23%"
             "24%25%26%27%28%29%2A%2B%2C-.%2F0123456789%3A%3B%3C%3D%3E%3F%40ABCDEFGHIJKLMNOPQRSTUVWXYZ%5B%5C%5D%5E_%"
