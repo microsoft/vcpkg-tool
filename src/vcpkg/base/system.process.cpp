fc9872a32cb7a058ae1a65f114e2250ccbb2c7f7
Javier Matos Denizac <javier.matosd@gmail.com>
2023-03-20 14:21:20 -0700

    print exit code

--- src/vcpkg/base/system.process.cpp
+++ src/vcpkg/base/system.process.cpp
@@ -1148,9 +1148,7 @@ namespace vcpkg
     {
         if (auto exit = maybe_exit.get())
         {
-            msg::write_unlocalized_text_to_stdout(Color::none, "exit code: \n");
-            msg::write_unlocalized_text_to_stdout(Color::none, exit->output);
-            msg::write_unlocalized_text_to_stdout(Color::none, "\n");
+            msg::write_unlocalized_text_to_stdout(Color::none, "exit code: " + exit->exit_code);
             if (exit->exit_code == 0)
             {
                 return {std::move(exit->output), expected_left_tag};
