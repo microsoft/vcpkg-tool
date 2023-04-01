46c86c1353c7d82c3c5d2858ef214e17e75917c2
Billy Robert O'Neal III <bion@microsoft.com>
2023-03-31 23:16:18 -0700

    Fixup PRDiff and signing.yml.

--- azure-pipelines/Create-PRDiff.ps1
+++ azure-pipelines/Create-PRDiff.ps1
@@ -4,7 +4,7 @@ Param(
     [String]$DiffFile
 )
 
-& git diff ':!vcpkg-artifacts/.npmrc' --output $DiffFile
+& git diff --output $DiffFile -- ':!vcpkg-artifacts/.npmrc'
 if (0 -ne (Get-Item -LiteralPath $DiffFile).Length)
 {
     Write-Error 'The formatting of the files in the repo were not what we expected, or you forgot to regenerate messages files. Please access the diff from format.diff in the build artifacts, and apply the patch with `git apply`'
