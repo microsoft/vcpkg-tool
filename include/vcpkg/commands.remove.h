680edaeb7058cb888122d298b51bfff08b10f228
Robert Schumacher <roschuma@microsoft.com>
2023-04-25 19:39:17 -0700

    Fix formatting

--- include/vcpkg/commands.remove.h
+++ include/vcpkg/commands.remove.h
@@ -5,7 +5,9 @@
 #include <vcpkg/fwd/dependencies.h>
 #include <vcpkg/fwd/installedpaths.h>
 #include <vcpkg/fwd/packagespec.h>
+#include <vcpkg/fwd/triplet.h>
 #include <vcpkg/fwd/vcpkgcmdarguments.h>
+#include <vcpkg/fwd/vcpkgpaths.h>
 
 namespace vcpkg::Remove
 {
@@ -16,14 +18,14 @@ namespace vcpkg::Remove
     };
 
     void remove_package(Filesystem& fs,
-        const InstalledPaths& installed,
-        const PackageSpec& spec,
-        StatusParagraphs& status_db);
+                        const InstalledPaths& installed,
+                        const PackageSpec& spec,
+                        StatusParagraphs& status_db);
 
     extern const CommandStructure COMMAND_STRUCTURE;
 
     void perform_and_exit(const VcpkgCmdArguments& args,
-        const VcpkgPaths& paths,
-        Triplet default_triplet,
-        Triplet host_triplet);
+                          const VcpkgPaths& paths,
+                          Triplet default_triplet,
+                          Triplet host_triplet);
 }
