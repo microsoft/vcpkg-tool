b666f94161a908237063ad2a2bb8c6cd3b573856
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2023-05-31 19:50:22 +0200

    Add missing files

---
+++ include/vcpkg/base/batch-quere.h
@@ -0,0 +1,57 @@
+#pragma once
+
+template<class T>
+class BatchQuere
+{
+public:
+    template<class... Args>
+    void push(Args&&... args)
+    {
+        forward.emplace_back(std::forward<Args>(args)...);
+    }
+
+    bool empty() const { return forward.empty(); }
+
+    void pop(std::vector<T>& out)
+    {
+        out.clear();
+        swap(out, forward);
+    }
+
+private:
+    std::vector<T> forward;
+};
+
+template<class WorkItem>
+struct BGThreadBatchQueue
+{
+    template<class... Args>
+    void push(Args&&... args)
+    {
+        std::lock_guard<std::mutex> lock(m_mtx);
+        m_tasks.push(std::forward<Args>(args)...);
+        m_cv.notify_all();
+    }
+
+    void wait_for_items(std::vector<WorkItem>& out)
+    {
+        std::unique_lock<std::mutex> lock(m_mtx);
+        m_cv.wait(lock, [this]() { return !m_tasks.empty() || !m_running; });
+        m_tasks.pop(out);
+    }
+
+    void stop()
+    {
+        std::lock_guard<std ::mutex> lock(m_mtx);
+        m_running = false;
+        m_cv.notify_all();
+    }
+
+    bool stopped() const { return m_running; }
+
+private:
+    std::mutex m_mtx;
+    std::condition_variable m_cv;
+    BatchQuere<WorkItem> m_tasks;
+    bool m_running = true;
+};
