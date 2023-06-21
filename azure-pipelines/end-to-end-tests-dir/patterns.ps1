ccd4f99334c73cc0d8ad72713097fb7ac12522c5
Billy Robert O'Neal III <bion@microsoft.com>
2023-06-20 19:41:37 -0700

    Fix patterns test writing into the source tree.

--- azure-pipelines/end-to-end-tests-dir/patterns.ps1
+++ azure-pipelines/end-to-end-tests-dir/patterns.ps1
@@ -1,84 +1,94 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
 
-### <Initialize registry>
-# Creates a git registry to run the e2e tests on
-$e2eProjects = "$PSScriptRoot/../e2e_projects"
-$manifestRoot = "$e2eProjects/registries-package-patterns"
-$e2eRegistryPath = "$PSScriptRoot/../e2e_registry".Replace('\', '\\')
-Push-Location $e2eRegistryPath
 try
 {
-	Write-Host "Initializing test registry"
-	if (Test-Path "$e2eRegistryPath/.git")
-	{
-		Remove-Item -Recurse -Force "$e2eRegistryPath/.git"
-	}
-	
+    Copy-Item -Recurse -LiteralPath @(
+        "$PSScriptRoot/../e2e_projects/registries-package-patterns",
+        "$PSScriptRoot/../e2e_registry"
+        ) $WorkingRoot
 
-	$gitConfig = @(
-		'-c', 'user.name=Nobody',
-		'-c', 'user.email=nobody@example.com',
-		'-c', 'core.autocrlf=false'
-	)
+    $manifestRoot = "$WorkingRoot/registries-package-patterns"
+    $e2eRegistryPath = "$WorkingRoot/e2e_registry".Replace('\', '\\')
+    Push-Location $e2eRegistryPath
+    ### <Initialize registry>
+    # Creates a git registry to run the e2e tests on
+    try
+    {
+        Write-Host "Initializing test registry"
+        if (Test-Path "$e2eRegistryPath/.git")
+        {
+            Remove-Item -Recurse -Force "$e2eRegistryPath/.git"
+        }
+        
 
-	git @gitConfig init . | Out-Null
-	Throw-IfFailed
-	git @gitConfig add -A | Out-Null
-	Throw-IfFailed
-	git @gitConfig commit -m "initial commit" | Out-Null
-	Throw-IfFailed
-	$e2eRegistryBaseline = git rev-parse HEAD
-	Throw-IfFailed
+        $gitConfig = @(
+            '-c', 'user.name=Nobody',
+            '-c', 'user.email=nobody@example.com',
+            '-c', 'core.autocrlf=false'
+        )
+
+        git @gitConfig init . | Out-Null
+        Throw-IfFailed
+        git @gitConfig add -A | Out-Null
+        Throw-IfFailed
+        git @gitConfig commit -m "initial commit" | Out-Null
+        Throw-IfFailed
+        $e2eRegistryBaseline = git rev-parse HEAD
+        Throw-IfFailed
+    }
+    finally 
+    {
+        Pop-Location
+    }
+    ### </Initialize Registry>
+
+    function Update-VcpkgJson {
+        param($PreReplacementName)
+        $content = Get-Content -LiteralPath "$manifestRoot/$PreReplacementName"
+        $content = $content.Replace('$E2ERegistryPath', $e2eRegistryPath)
+        $content = $content.Replace('$E2ERegistryBaseline', $e2eRegistryBaseline)
+        Set-Content -LiteralPath "$manifestRoot/vcpkg.json" -Value $content
+    }
+
+    $commonArgs += @("--x-manifest-root=$manifestRoot")
+
+    # [patterns] No patterns (no default)
+    Write-Host "[patterns] No patterns (no default)"
+    Update-VcpkgJson 'no-patterns.json.in'
+    Run-Vcpkg -EndToEndTestSilent @commonArgs install | Out-Null
+    Throw-IfFailed
+    Refresh-TestRoot
+
+    # [patterns] Patterns only (no default)
+    Write-Host "[patterns] Patterns only (no default)"
+    Update-VcpkgJson 'only-patterns.json.in'
+    Run-Vcpkg -EndToEndTestSilent @commonArgs install | Out-Null
+    Throw-IfFailed
+    Refresh-TestRoot
+
+    # [patterns] Patterns with default
+    Write-Host "[patterns] Patterns with default"
+    Update-VcpkgJson 'with-default.json.in'
+    Run-Vcpkg -EndToEndTestSilent @commonArgs install | Out-Null
+    Throw-IfFailed
+    Refresh-TestRoot
+
+    # [patterns] Repeated patterns
+    Write-Host "[patterns] Repeated patterns"
+    Update-VcpkgJson 'with-redeclaration.json.in'
+    $out = Run-VcpkgAndCaptureOutput -EndToEndTestSilent @commonArgs install
+    Throw-IfFailed
+    if ($out -notmatch "redeclarations will be ignored")
+    {
+        $out
+        throw "Expected warning about redeclaration"
+    }
+    Refresh-TestRoot
 }
-finally 
+finally
 {
-	Pop-Location
+    Remove-Item -Recurse -Force -LiteralPath @(
+        "$WorkingRoot/registries-package-patterns",
+        "$WorkingRoot/e2e_registry"
+        ) -ErrorAction SilentlyContinue
 }
-### </Initialize Registry>
-
-$commonArgs += @("--x-manifest-root=$manifestRoot")
-
-# [patterns] No patterns (no default)
-Write-Host "[patterns] No patterns (no default)"
-$inFile = "$manifestRoot/no-patterns.json.in"
-(Get-Content -Path "$inFile").Replace("`$E2ERegistryPath", $e2eRegistryPath).Replace("`$E2ERegistryBaseline", $e2eRegistryBaseline) `
-| Out-File "$manifestRoot/vcpkg.json"
-
-Run-Vcpkg -EndToEndTestSilent @commonArgs install | Out-Null
-Throw-IfFailed
-Refresh-TestRoot
-
-# [patterns] Patterns only (no default)
-Write-Host "[patterns] Patterns only (no default)"
-$inFile = "$manifestRoot/only-patterns.json.in"
-(Get-Content -Path "$inFile").Replace("`$E2ERegistryPath", $e2eRegistryPath).Replace("`$E2ERegistryBaseline", $e2eRegistryBaseline) `
-| Out-File "$manifestRoot/vcpkg.json"
-
-Run-Vcpkg -EndToEndTestSilent @commonArgs install | Out-Null
-Throw-IfFailed
-Refresh-TestRoot
-
-# [patterns] Patterns with default
-Write-Host "[patterns] Patterns with default"
-$inFile = "$manifestRoot/with-default.json.in"
-(Get-Content -Path "$inFile").Replace("`$E2ERegistryPath", $e2eRegistryPath).Replace("`$E2ERegistryBaseline", $e2eRegistryBaseline) `
-| Out-File "$manifestRoot/vcpkg.json"
-
-Run-Vcpkg -EndToEndTestSilent @commonArgs install | Out-Null
-Throw-IfFailed
-Refresh-TestRoot
-
-# [patterns] Repeated patterns
-Write-Host "[patterns] Repeated patterns"
-$inFile = "$manifestRoot/with-redeclaration.json.in"
-(Get-Content -Path "$inFile").Replace("`$E2ERegistryPath", $e2eRegistryPath).Replace("`$E2ERegistryBaseline", $e2eRegistryBaseline) `
-| Out-File "$manifestRoot/vcpkg.json"
-
-$out = Run-VcpkgAndCaptureOutput -EndToEndTestSilent @commonArgs install
-Throw-IfFailed
-if ($out -notmatch "redeclarations will be ignored")
-{
-	$out
-	throw "Expected warning about redeclaration"
-}
-Refresh-TestRoot
