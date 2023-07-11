6954542b26d7a0827cab553a5f07ef8b706cc951
Javier Matos Denizac <javier.matosd@gmail.com>
2023-07-11 14:12:08 -0700

    test executable bits on non-windows

--- azure-pipelines/end-to-end-tests-dir/commands.extract.ps1
+++ azure-pipelines/end-to-end-tests-dir/commands.extract.ps1
@@ -1,1 +1,12 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
+
+$out = Join-Path $TestingRoot
+Run-Vcpkg z-extract "$PSScriptRoot/../e2e-assets/extract/a-tar-with-plus-x.tar.gz" $out
+Throw-IfFailed
+
+if (-Not $IsWindows) {
+    $extractedFilePath = Join-Path $out "myExe"
+    if (-Not $(test -x $extractedFilePath)) { # Check if the file is executable
+        throw "File does not have +x permission"
+    }
+}
