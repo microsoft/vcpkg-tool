b179bbc29da7c83852e76f901408dd8c03c088be
Javier Matos Denizac <javier.matosd@gmail.com>
2023-07-11 22:33:01 +0000

    print UnixMode

--- azure-pipelines/end-to-end-tests-dir/commands.extract.ps1
+++ azure-pipelines/end-to-end-tests-dir/commands.extract.ps1
@@ -4,14 +4,14 @@ $out = Join-Path $TestingRoot "a-tar-with-execute"
 Run-Vcpkg z-extract "$PSScriptRoot/../e2e-assets/extract/a-tar-with-plus-x.tar.gz" $out
 Throw-IfFailed
 
+if (-Not (Test-Path $extractedFilePath)) {
+    throw "Extraction Failed"
+}
+
 if (-Not $IsWindows) {
     $extractedFilePath = Join-Path $out "myExe"
-
-    if (-Not (Test-Path $extractedFilePath)) {
-        throw "File does not exist"
-    }
-    
-    if ((Get-Item $extractedFilePath).UnixMode -ne "-rwxrwxrwx") {
-        throw "File does not have +x permission"
+    $unixMode = (Get-Item $extractedFilePath).UnixMode
+    if ($unixMode -ne "-rwxrwxrwx") {
+        throw "File does not have +x permission. UnixMode: $unixMode"
     }
 }
