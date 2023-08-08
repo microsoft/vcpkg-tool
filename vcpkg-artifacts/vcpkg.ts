dbecad216cbbb5f78fe40f554c10fe8fc7ab3c2f
Javier Matos Denizac <javier.matosd@gmail.com>
2023-08-08 16:08:08 -0700

    strip is optional argument

--- vcpkg-artifacts/vcpkg.ts
+++ vcpkg-artifacts/vcpkg.ts
@@ -44,8 +44,8 @@ export function vcpkgFetch(session: Session, fetchKey: string): Promise<string> 
   });
 }
 
-export async function vcpkgExtract(session: Session, archive: string, target:string, strip:string): Promise<string> {
-  return runVcpkg(session.vcpkgCommand, ['z-extract', archive, target, strip]);
+export async function vcpkgExtract(session: Session, archive: string, target:string, strip?:string): Promise<string> {
+  return runVcpkg(session.vcpkgCommand, strip ? ['z-extract', archive, target, strip] : ['z-extract', archive, target]);
 }
 
 export async function vcpkgDownload(session: Session, destination: string, sha512: string | undefined, uris: Array<Uri>, events: Partial<DownloadEvents>) : Promise<void> {
