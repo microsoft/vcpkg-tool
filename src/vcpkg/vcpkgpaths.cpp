d1c1423ef5ff0750aed9c5075b1319d269230d36
Billy O'Neal <bion@microsoft.com>
2023-08-08 17:23:31 -0700

    Fix crash trying to make a github issue report caused by compiler metadata being missing. (#1138)

--- src/vcpkg/vcpkgpaths.cpp
+++ src/vcpkg/vcpkgpaths.cpp
@@ -1338,19 +1338,19 @@ namespace vcpkg
 #endif
     }
 
-    const Environment& VcpkgPaths::get_action_env(const AbiInfo& abi_info) const
+    const Environment& VcpkgPaths::get_action_env(const PreBuildInfo& pre_build_info, const Toolset& toolset) const
     {
-        return m_pimpl->m_env_cache.get_action_env(*this, abi_info);
+        return m_pimpl->m_env_cache.get_action_env(*this, pre_build_info, toolset);
     }
 
-    const std::string& VcpkgPaths::get_triplet_info(const AbiInfo& abi_info) const
+    const std::string& VcpkgPaths::get_triplet_info(const PreBuildInfo& pre_build_info, const Toolset& toolset) const
     {
-        return m_pimpl->m_env_cache.get_triplet_info(*this, abi_info);
+        return m_pimpl->m_env_cache.get_triplet_info(*this, pre_build_info, toolset);
     }
 
-    const CompilerInfo& VcpkgPaths::get_compiler_info(const AbiInfo& abi_info) const
+    const CompilerInfo& VcpkgPaths::get_compiler_info(const PreBuildInfo& pre_build_info, const Toolset& toolset) const
     {
-        return m_pimpl->m_env_cache.get_compiler_info(*this, abi_info);
+        return m_pimpl->m_env_cache.get_compiler_info(*this, pre_build_info, toolset);
     }
 
     const FeatureFlagSettings& VcpkgPaths::get_feature_flags() const { return m_pimpl->m_ff_settings; }
