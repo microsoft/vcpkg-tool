a100ac74e226df8e0c60254d83ff1ca799b319e8
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2023-08-16 00:18:05 +0200

    Update src/vcpkg/commands.cache.cpp
    Co-authored-by: Billy O'Neal <bion@microsoft.com>

--- src/vcpkg/commands.cache.cpp
+++ src/vcpkg/commands.cache.cpp
@@ -16,7 +16,7 @@ namespace vcpkg::Commands::Cache
         for (auto&& path : paths.get_filesystem().get_files_non_recursive(paths.packages(), VCPKG_LINE_INFO))
         {
             auto pghs = Paragraphs::get_single_paragraph(paths.get_filesystem(), std::move(path) / "CONTROL");
-            if (auto p = pghs.get())
+            if (const auto p = pghs.get())
             {
                 output.emplace_back(std::move(*p));
             }
