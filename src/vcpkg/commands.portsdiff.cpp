0dd96eec2308f31edf5b2c8f227c17d469e4cf72
Billy Robert O'Neal III <bion@microsoft.com>
2023-10-02 11:45:07 -0700

    clang-format

--- src/vcpkg/commands.portsdiff.cpp
+++ src/vcpkg/commands.portsdiff.cpp
@@ -82,7 +82,7 @@ namespace
         auto& fs = paths.get_filesystem();
         const auto dot_git_dir = fs.try_find_file_recursively_up(paths.builtin_ports_directory().parent_path(), ".git")
                                      .map([](Path&& dot_git_parent) { return std::move(dot_git_parent) / ".git"; })
-                                     .value_or_exit(VCPKG_LINE_INFO);;
+                                     .value_or_exit(VCPKG_LINE_INFO);
         const auto ports_dir_name = paths.builtin_ports_directory().filename();
         const auto temp_checkout_path = paths.buildtrees() / fmt::format("{}-{}", ports_dir_name, git_commit_id);
         fs.create_directory(temp_checkout_path, IgnoreErrors{});
