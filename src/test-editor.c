9601b7910210e0aa16ea67f3785f32c3e979c763
Billy O'Neal <bion@microsoft.com>
2023-10-05 20:22:51 -0700

    Fix edit command. (#1221)

---
+++ src/test-editor.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char* argv[])
+{
+    const char* path;
+    FILE* f;
+
+    path = getenv("VCPKG_TEST_OUTPUT");
+    if (!path)
+    {
+        puts("bad env var");
+        return 1;
+    }
+
+    f = fopen(path, "wb");
+    if (!f)
+    {
+        puts("bad open");
+        return 1;
+    }
+
+    for (int idx = 1; idx < argc; ++idx)
+    {
+        if (fputs(argv[idx], f) < 0)
+        {
+            puts("bad write");
+        }
+
+        if (fputs("\n", f) < 0)
+        {
+            puts("bad write newline");
+        }
+    }
+
+    fclose(f);
+}
