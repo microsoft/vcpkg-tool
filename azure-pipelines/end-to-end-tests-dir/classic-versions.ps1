ea1b965c17b810e2891fddea42eb69cb8dffd55f
Billy Robert O'Neal III <bion@microsoft.com>
2023-10-31 20:06:46 -0700

    Add test case for the recording head version feature noted in https://github.com/microsoft/vcpkg-tool/pull/1259#discussion_r1378120355

--- azure-pipelines/end-to-end-tests-dir/classic-versions.ps1
+++ azure-pipelines/end-to-end-tests-dir/classic-versions.ps1
@@ -1,9 +1,28 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
 
 # Not a number
+Refresh-TestRoot
 $out = Run-VcpkgAndCaptureOutput @commonArgs install classic-versions-b
 Throw-IfNotFailed
 if ($out -notmatch ".*warning:.*dependency classic-versions-a.*at least version 2.*is currently 1.*")
 {
     throw "Expected to fail and print warning about mismatched versions"
+}
+
+Refresh-TestRoot
+Run-Vcpkg @commonArgs install set-detected-head-version
+Throw-IfFailed
+$listResults = Run-VcpkgAndCaptureOutput @commonArgs list
+if (-Not ($listResults.Trim() -match "set-detected-head-version:[\S]+[\s]+1.0.0"))
+{
+    throw "Expected list to list the declared version"
+}
+
+Refresh-TestRoot
+Run-Vcpkg @commonArgs install set-detected-head-version --head
+Throw-IfFailed
+$listResults = Run-VcpkgAndCaptureOutput @commonArgs list
+if (-Not ($listResults.Trim() -match "set-detected-head-version:[\S]+[\s]+detected-head"))
+{
+    throw "Expected list to list the declared version"
 }
