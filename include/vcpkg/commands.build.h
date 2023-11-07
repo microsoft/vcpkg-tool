c9e4be1d6adfcc2801179a73e5864d332d74f9a1
autoantwort <41973254+autoantwort@users.noreply.github.com>
2023-11-07 20:53:01 +0100

    Ensure generated GitHub issue bodies are not too long to post (#1257)

--- include/vcpkg/commands.build.h
+++ include/vcpkg/commands.build.h
@@ -186,12 +186,16 @@ namespace vcpkg
         std::vector<std::string> error_logs;
     };
 
+    void append_log(const Path& path, const std::string& log, size_t max_size, std::string& out);
+    void append_logs(std::vector<std::pair<Path, std::string>>&& logs, size_t max_size, std::string& out);
+
     LocalizedString create_error_message(const ExtendedBuildResult& build_result, const PackageSpec& spec);
 
     std::string create_github_issue(const VcpkgCmdArguments& args,
                                     const ExtendedBuildResult& build_result,
                                     const VcpkgPaths& paths,
-                                    const InstallPlanAction& action);
+                                    const InstallPlanAction& action,
+                                    bool include_manifest);
 
     ExtendedBuildResult build_package(const VcpkgCmdArguments& args,
                                       const VcpkgPaths& paths,
