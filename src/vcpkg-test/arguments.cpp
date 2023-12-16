754ef54c55b7e55bddc0be98a281d6c2b7fe1680
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2023-12-16 15:56:59 +0000

    fix unit tests

--- src/vcpkg-test/arguments.cpp
+++ src/vcpkg-test/arguments.cpp
@@ -161,7 +161,7 @@ TEST_CASE ("Combine asset cache params", "[arguments]")
     REQUIRE(v.asset_sources_template() == "x-azurl,value");
 
     std::map<StringLiteral, std::string, std::less<>> envmap = {
-        {VcpkgCmdArguments::ASSET_SOURCES_ENV.to_string(), "x-azurl,value1"},
+        {VcpkgCmdArguments::ASSET_SOURCES_ENV, "x-azurl,value1"},
     };
     v = VcpkgCmdArguments::create_from_arg_sequence(nullptr, nullptr);
     v.imbue_from_fake_environment(envmap);
