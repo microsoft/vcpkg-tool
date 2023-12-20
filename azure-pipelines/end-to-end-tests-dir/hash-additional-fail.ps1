7050ce1ad2e484a9800922084873e5e4d4c313a3
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-12-20 10:27:38 +0100

    Test-Match is only available for artifact tests

--- azure-pipelines/end-to-end-tests-dir/hash-additional-fail.ps1
+++ azure-pipelines/end-to-end-tests-dir/hash-additional-fail.ps1
@@ -1,4 +1,7 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
 $output = Run-VcpkgAndCaptureOutput "--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional-fail" "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional-fail" install vcpkg-test-hash-additional --triplet hash-additional-e2e 
 Throw-IfNotFailed
-Test-Match $output "Variable VCPKG_HASH_ADDITIONAL_FILES contains invalid file path"
+if ($output -notmatch "Variable VCPKG_HASH_ADDITIONAL_FILES contains invalid file path")
+{
+    throw "Expected to fail since VCPKG_HASH_ADDITIONAL_FILES is set to a relative path"
+}
