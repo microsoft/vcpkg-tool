669fbda8c5c6b85d08760b4bc28b520fca9e5410
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-12-20 01:14:33 +0100

    Make the failure case actually capture output

--- azure-pipelines/end-to-end-tests-dir/hash-additional-fail.ps1
+++ azure-pipelines/end-to-end-tests-dir/hash-additional-fail.ps1
@@ -1,3 +1,4 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
-Run-Vcpkg "--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional-fail" "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional-fail" install vcpkg-test-hash-additional --triplet hash-additional-e2e 
+$output = Run-VcpkgAndCaptureOutput "--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional-fail" "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional-fail" install vcpkg-test-hash-additional --triplet hash-additional-e2e 
 Throw-IfNotFailed
+Test-Match $output "Variable VCPKG_HASH_ADDITIONAL_FILES contains invalid file path"
