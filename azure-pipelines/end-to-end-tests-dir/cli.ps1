ee2b7db59e5afb5d11542d799b8daee8dc1e1fdd
Billy Robert O'Neal III <bion@microsoft.com>
2024-01-10 15:25:10 -0800

    Add end to end tests.

--- azure-pipelines/end-to-end-tests-dir/cli.ps1
+++ azure-pipelines/end-to-end-tests-dir/cli.ps1
@@ -10,6 +10,42 @@ Throw-IfNotFailed
 Run-Vcpkg -TestArgs ($commonArgs + @("install", "vcpkg-hello-world-1", "--fast")) # --fast is not a switch
 Throw-IfNotFailed
 
+# Test switching stdout vs. stderr for help topics and errors
+[string]$out = Run-VcpkgAndCaptureOutput -TestArgs @('help')
+Throw-IfFailed
+if (-Not ($out.StartsWith('usage: vcpkg <command> [--switches] [--options=values] [arguments] @response_file')))
+{
+    throw 'Bad help output'
+}
+
+$out = Run-VcpkgAndCaptureOutput -TestArgs @('help', 'help')
+Throw-IfFailed
+if (-Not ($out.StartsWith('Synopsis: Displays specific help topic')))
+{
+    throw 'Bad help help output'
+}
+
+$out = Run-VcpkgAndCaptureStdErr -TestArgs @('help', 'not-a-topic')
+Throw-IfNotFailed
+if (-Not ($out.StartsWith('error: unknown topic not-a-topic')))
+{
+    throw 'Bad help not-a-topic output'
+}
+
+$out = Run-VcpkgAndCaptureStdErr -TestArgs @('not-a-command')
+Throw-IfNotFailed
+if (-Not ($out.StartsWith('error: invalid command: not-a-command')))
+{
+    throw 'Bad not-a-command output'
+}
+
+$out = Run-VcpkgAndCaptureStdErr -TestArgs @('install', '--not-a-switch')
+Throw-IfNotFailed
+if (-Not ($out.StartsWith('error: unexpected switch: --not-a-switch')))
+{
+    throw 'Bad install --not-a-switch output'
+}
+
 if ($IsWindows) {
     $warningText = 'In the September 2023 release'
 
