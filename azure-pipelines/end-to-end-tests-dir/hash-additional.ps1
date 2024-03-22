5d9be8000c932a03ca5e512fe2d2ba123db16ba5
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2024-03-22 11:14:10 +0000

    try again

--- azure-pipelines/end-to-end-tests-dir/hash-additional.ps1
+++ azure-pipelines/end-to-end-tests-dir/hash-additional.ps1
@@ -1,6 +1,6 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
 
-[string]$out = Run-VcpkgAndCaptureOutput -TestArgs @("help", "--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional", "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional", "install", "vcpkg-test-hash-additional", "--triplet", "hash-additional-e2e", "--debug")
+[string]$out = Run-VcpkgAndCaptureOutput -TestArgs @("--overlay-triplets=$PSScriptRoot/../e2e_ports/hash-additional", "--overlay-ports=$PSScriptRoot/../e2e_ports/hash-additional", "x-set-installed", "vcpkg-test-hash-additional", "--triplet", "hash-additional-e2e", "--debug")
 Throw-IfFailed
 if (-Not ($out.Contains("additional_file_0|61ba0c7fc1f696e28c1b7aa9460980a571025ff8c97bb90a57e990463aa25660")))
 {
