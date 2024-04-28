bb8f6b6781cda21e860c27ac70962d4bbbcd04f5
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2024-04-28 20:15:33 +0200

    Finish Console implementation

--- include/vcpkg/base/system.console.h
+++ include/vcpkg/base/system.console.h
@@ -8,29 +8,43 @@
 
 #include <mutex>
 
-#include <unistd.h>
-
 namespace vcpkg
 {
     class Console
     {
     public:
-        explicit Console(int fd) noexcept : fd(fd), is_terminal(::isatty(fd) == 1) {}
+#ifdef _WIN32
+        explicit Console(unsigned long std_device) noexcept;
+#else
+        explicit Console(int fd) noexcept;
+#endif
         ~Console() = default;
 
+        // This function is safe to call from multiple threads.
+        // When called from multiple threads, calls are atomic with respect to other callers
         void print(Color color, StringView text)
         {
             std::lock_guard<std::mutex> lck{mtx};
             print_unlocked(color, text);
+            flush();
         }
 
+        // This function is safe to call from multiple threads.
+        // When called from multiple threads, calls are atomic with respect to other callers
         void print_lines(View<std::pair<Color, StringView>> lines);
+
+        // This function is safe to call from multiple threads.
+        // When called from multiple threads, calls are atomic with respect to other callers
         void println(Color color, StringView text)
         {
             std::lock_guard<std::mutex> lck{mtx};
             print_unlocked(color, text);
             print_unlocked(Color::none, "\n");
+            flush();
         }
+
+        // This function is safe to call from multiple threads.
+        // When called from multiple threads, calls are atomic with respect to other callers
         void println(Color color, std::string&& text)
         {
             text.push_back('\n');
@@ -40,10 +54,15 @@ namespace vcpkg
     private:
         void print_unlocked(Color color, StringView text) noexcept;
         void write(const char* text, size_t count) noexcept;
+        void flush() noexcept;
+        bool check_is_terminal() noexcept;
 
         std::mutex mtx;
+#if _WIN32
+        void* fd;
+#else
         int fd;
+#endif
         bool is_terminal;
     };
 } // namespace vcpkg
-
--- src/vcpkg/base/system.console.cpp
+++ src/vcpkg/base/system.console.cpp
@@ -1,9 +1,45 @@
+#include <vcpkg/base/span.h>
 #include <vcpkg/base/stringview.h>
 #include <vcpkg/base/system.console.h>
-#include <vcpkg/base/span.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 namespace vcpkg
 {
+#ifdef _WIN32
+    Console::Console(unsigned long std_device) noexcept
+        : fd(::GetStdHandle(std_device)), is_terminal(this->check_is_terminal())
+    {
+    }
+#else
+    Console::Console(int fd) noexcept : fd(fd), is_terminal(this->check_is_terminal()) { }
+#endif
+
+    void Console::flush() noexcept
+    {
+#ifdef _WIN32
+        ::FlushFileBuffers(fd);
+#else
+        ::fflush(fd);
+#endif
+    }
+
+    bool Console::check_is_terminal() noexcept
+    {
+#ifdef _WIN32
+        DWORD mode = 0;
+        // GetConsoleMode succeeds if `h` is a console
+        // we do not actually care about the mode of the console
+        return GetConsoleMode(fd, &mode);
+#else
+        return ::isatty(fd) == 1;
+#endif
+    }
+
     void Console::print_lines(View<std::pair<Color, StringView>> lines)
     {
         std::lock_guard<std::mutex> lck{mtx};
@@ -14,13 +50,57 @@ namespace vcpkg
         }
     }
 
+#ifdef _WIN32
+    void Console::print_unlocked(Color c, StringView sv) noexcept
+    {
+        if (sv.empty()) return;
+
+        WORD original_color = 0;
+
+        if (is_terminal && c != Color::none)
+        {
+            CONSOLE_SCREEN_BUFFER_INFO console_screen_buffer_info{};
+            ::GetConsoleScreenBufferInfo(fd, &console_screen_buffer_info);
+            original_color = console_screen_buffer_info.wAttributes;
+            ::SetConsoleTextAttribute(fd, static_cast<WORD>(c) | (original_color & 0xF0));
+        }
+
+        write(sv.data(), sv.size());
+
+        if (is_terminal && c != Color::none)
+        {
+            ::SetConsoleTextAttribute(fd, original_color);
+        }
+    }
+
+    static constexpr DWORD size_to_write(size_t size) noexcept
+    {
+        return size > MAXDWORD ? MAXDWORD : static_cast<DWORD>(size);
+    }
+
+    void Console::write(const char* text, size_t count) noexcept
+    {
+        while (count != 0)
+        {
+            DWORD written = 0;
+            if (::WriteFile(fd, text, size_to_write(count), &written, nullptr))
+            {
+                ::fwprintf(stderr, L"[DEBUG] Failed to write to stdout: %lu\n", GetLastError());
+                std::abort();
+            }
+            text += written;
+            count -= written;
+        }
+    }
+
+#else
+
     void Console::print_unlocked(Color c, StringView sv) noexcept
     {
         static constexpr char reset_color_sequence[] = {'\033', '[', '0', 'm'};
 
         if (sv.empty()) return;
 
-        std::lock_guard<std::mutex> lck{mtx};
         bool reset_color = false;
         // Only write color sequence if file descriptor is a terminal
         if (is_terminal && c != Color::none)
@@ -53,11 +133,15 @@ namespace vcpkg
             count -= written;
         }
     }
-
+#endif
+#ifdef _WIN32
+    Console std_out_instance(STD_OUTPUT_HANDLE);
+    Console std_error_instance(STD_ERROR_HANDLE);
+#else
     Console std_out_instance(STDOUT_FILENO);
     Console std_error_instance(STDERR_FILENO);
+#endif
 
     Console& std_out = std_out_instance;
     Console& std_error = std_out_instance;
 } // namespace vcpkg
-
