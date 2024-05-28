9676b5345c134b415bfcb9f6d0f158bd92ca0fc0
Javier Matos <javiermatos@Javiers-Laptop.local>
2024-05-28 15:14:18 -0400

    add e2e tests

--- azure-pipelines/end-to-end-tests-dir/asset-caching.ps1
+++ azure-pipelines/end-to-end-tests-dir/asset-caching.ps1
@@ -1,6 +1,105 @@
 . $PSScriptRoot/../end-to-end-tests-prelude.ps1
 
+# Testing x-script
 Refresh-TestRoot
 Run-Vcpkg -TestArgs ($commonArgs + @('fetch', 'cmake'))
 Run-Vcpkg -TestArgs ($commonArgs + @("install", "vcpkg-test-x-script", "--x-binarysource=clear", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=x-script,$TestScriptAssetCacheExe {url} {sha512} {dst};x-block-origin"))
 Throw-IfFailed
+
+# Test Asset Cache not configured + x-block-origin enabled
+Refresh-TestRoot
+$actual = Run-VcpkgAndCaptureOutput -TestArgs ($commonArgs + @("install", "vcpkg-internal-e2e-test-port", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=clear;x-block-origin", "--downloads-root=$DownloadsRoot"))
+
+$expected = @(
+"Computing installation plan..."
+"A suitable version of cmake was not found \(required v[0-9\.]+\)."
+"error: Failed to download cmake-[0-9\.]+-.*\.tar\.gz\."
+) -join "`n"
+
+if (-not ($actual -match $expected)) {
+    throw "Asset cache not configured + x-block-origin enabled failed"
+}
+
+# Test Asset Cache not configured + x-block-origin disabled
+$actual = Run-VcpkgAndCaptureOutput -TestArgs ($commonArgs + @("install", "vcpkg-internal-e2e-test-port", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=clear;", "--downloads-root=$DownloadsRoot"))
+
+$expected = @(
+"Computing installation plan..."
+"A suitable version of cmake was not found \(required v[0-9\.]+\)."
+"Downloading .*"
+"warning: Asset Caching is not configured. Failed to store cmake-[0-9\.]+-.*\.tar\.gz to mirror."
+"Extracting cmake..."
+) -join "`n"
+
+if (-not ($actual -match $expected)) {
+    throw "Asset cache not configured + x-block-origin disabled failed"
+}
+
+# Test Asset Cache Miss + x-block-origin enabled
+Refresh-TestRoot
+$actual = Run-VcpkgAndCaptureOutput -TestArgs ($commonArgs + @("install", "vcpkg-internal-e2e-test-port", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=x-azurl,file://$AssetCache,,readwrite;x-block-origin", "--downloads-root=$DownloadsRoot"))
+
+$expected = @(
+"Computing installation plan..."
+"A suitable version of cmake was not found \(required v[0-9\.]+\)."
+"Asset cache miss for cmake-[0-9\.]+-.*\.tar\.gz."
+"error: Failed to download cmake-[0-9\.]+-.*\.tar\.gz."
+) -join "`n"
+
+if (-not ($actual -match $expected)) {
+    throw "Asset cache miss + x-block-origin enabled failed"
+}
+
+# Test Asset Cache Miss + x-block-origin disabled
+$actual = Run-VcpkgAndCaptureOutput -TestArgs ($commonArgs + @("install", "vcpkg-internal-e2e-test-port", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=x-azurl,file://$AssetCache,,readwrite;", "--downloads-root=$DownloadsRoot"))
+
+$expected = @(
+"Computing installation plan..."
+"A suitable version of cmake was not found \(required v[0-9\.]+\)."
+"Asset cache miss for cmake-[0-9\.]+-.*\.tar\.gz."
+"Downloading .*"
+"Successfully stored cmake-[0-9\.]+-.*\.tar\.gz to mirror."
+"Extracting cmake..."
+) -join "`n"
+
+if (-not ($actual -match $expected)) {
+    throw "Asset cache miss + x-block-origin disabled failed"
+}
+
+# Test Asset Cache Hit
+Refresh-Downloads
+Run-Vcpkg -TestArgs ($commonArgs + @('remove', 'vcpkg-internal-e2e-test-port'))
+$actual = Run-VcpkgAndCaptureOutput -TestArgs ($commonArgs + @("install", "vcpkg-internal-e2e-test-port", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=x-azurl,file://$AssetCache,,readwrite;", "--downloads-root=$DownloadsRoot"))
+$expected = @(
+"Computing installation plan..."
+"A suitable version of cmake was not found \(required v[0-9\.]+\)."
+"Asset cache hit for cmake-[0-9\.]+-.*\.tar\.gz."
+"Extracting cmake..."
+) -join "`n"
+
+if (-not ($actual -match $expected)) {
+    throw "Asset cache hit failed"
+}
+
+# Test asset caching && x-block-orgin promises when --debug is passed (enabled)
+Refresh-TestRoot
+$actual = Run-VcpkgAndCaptureOutput -TestArgs ($commonArgs + @("install", "vcpkg-internal-e2e-test-port", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=x-azurl,file://$AssetCache,,readwrite;x-block-origin", "--downloads-root=$DownloadsRoot", "--debug"))
+# Define the regex pattern that accounts for multiline input
+$expectedPattern = "(?s)" +
+                   ".*\[DEBUG\] External asset downloads are blocked \(x-block-origin is enabled\)\.\.?" +
+                   ".*\[DEBUG\] Asset caching is enabled\..*"
+
+if (-not ($actual -match $expectedPattern)) {
+    throw "Test failed: Debug messages mismatch"
+}
+
+# Test asset caching && x-block-orgin promises when --debug is passed (disabled)
+Refresh-TestRoot
+$actual = Run-VcpkgAndCaptureOutput -TestArgs ($commonArgs + @("install", "vcpkg-internal-e2e-test-port", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=clear", "--downloads-root=$DownloadsRoot", "--debug"))
+$expectedPattern = "(?s)" +
+                   ".*\[DEBUG\] External asset downloads are allowed \(x-block-origin is disabled\)\.\.\.?" +
+                   ".*\[DEBUG\] Asset cache is not configured.*"
+
+if (-not ($actual -match $expectedPattern)) {
+    throw "Test failed: Debug messages mismatch"
+}
