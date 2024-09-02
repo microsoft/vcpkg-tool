0e2fe306b8c92c5aa64e527323271eb1b3f1aaea
Tamás PEREGI <petamas@gmail.com>
2024-09-02 16:11:08 +0200

    Parallelize downloads

--- src/vcpkg/binarycaching.cpp
+++ src/vcpkg/binarycaching.cpp
@@ -7,6 +7,7 @@
 #include <vcpkg/base/json.h>
 #include <vcpkg/base/message_sinks.h>
 #include <vcpkg/base/messages.h>
+#include <vcpkg/base/parallel-algorithms.h>
 #include <vcpkg/base/parse.h>
 #include <vcpkg/base/strings.h>
 #include <vcpkg/base/system.debug.h>
@@ -1010,8 +1011,7 @@ namespace
         void acquire_zips(View<const InstallPlanAction*> actions,
                           Span<Optional<ZipResource>> out_zip_paths) const override
         {
-            for (size_t idx = 0; idx < actions.size(); ++idx)
-            {
+            execute_in_parallel(actions.size(), [&](size_t idx) {
                 auto&& action = *actions[idx];
                 const auto& abi = action.package_abi().value_or_exit(VCPKG_LINE_INFO);
                 auto tmp = make_temp_archive_path(m_buildtrees, action.spec);
@@ -1027,13 +1027,12 @@ namespace
                 {
                     out_sink.println_warning(res.error());
                 }
-            }
+            });
         }
 
         void precheck(View<const InstallPlanAction*> actions, Span<CacheAvailability> cache_status) const override
         {
-            for (size_t idx = 0; idx < actions.size(); ++idx)
-            {
+            execute_in_parallel(actions.size(), [&](size_t idx) {
                 auto&& action = *actions[idx];
                 const auto& abi = action.package_abi().value_or_exit(VCPKG_LINE_INFO);
                 auto maybe_res = m_tool->stat(make_object_path(m_prefix, abi));
@@ -1045,7 +1044,7 @@ namespace
                 {
                     cache_status[idx] = CacheAvailability::unavailable;
                 }
-            }
+            });
         }
 
         LocalizedString restored_message(size_t count,
