microsoft/vcpkg-tool

commit e2f895e3c4568978c5acf1fa49f1a4b1785fb786

Disallow invalid triplet names passed via `--triplet` (#1474)

src/vcpkg/input.cpp
@@ -1,6 +1,6 @@
-
 #include <vcpkg/base/checks.h>
 #include <vcpkg/base/messages.h>
+#include <vcpkg/base/parse.h>
 #include <vcpkg/base/strings.h>
 
 #include <vcpkg/commands.help.h>
@@ -26,6 +26,12 @@ namespace vcpkg
     {
         // Intentionally show the lowercased string
         auto as_lower = Strings::ascii_to_lowercase(name);
+
+        if (std::find_if_not(name.begin(), name.end(), ParserBase::is_package_name_char) != name.end())
+        {
+            return msg::format_error(msgParseQualifiedSpecifierNotEof);
+        }
+
         if (!database.is_valid_triplet_canonical_name(as_lower))
         {
             LocalizedString result = msg::format_error(msgInvalidTriplet, msg::triplet = as_lower);

src/vcpkg-test/input.cpp
@@ -105,6 +105,18 @@ See )" + docs::triplets_url + R"( for more information.
     REQUIRE(maybe_check.error() == expected_error);
 }
 
+TEST_CASE ("check_triplet rejects malformed triplet", "[input][check_triplet]")
+{
+    TripletDatabase db;
+    db.available_triplets.push_back(TripletFile{"invalid.triplet_name", "invalid.triplet_name.cmake"});
+    auto maybe_check = check_triplet("invalid.triplet_name", db);
+    REQUIRE(!maybe_check.has_value());
+    static constexpr StringLiteral expected_error{
+        "error: expected the end of input parsing a package spec; this usually means the indicated character is not "
+        "allowed to be in a package spec. Port, triplet, and feature names are all lowercase alphanumeric+hypens."};
+    REQUIRE(maybe_check.error() == expected_error);
+}
+
 TEST_CASE ("check_and_get_package_spec validates the triplet", "[input][check_and_get_package_spec]")
 {
     TripletDatabase db;

commit a7b45dfbdb7f9d60ed54bb199bdaa11774217534

Improve diagnostics when an invalid triplet name is supplied.
This fixes a regression introduced in https://github.com/microsoft/vcpkg-tool/pull/1474 where an error message is added but the corresponding text being parsed is not. This was reported as https://github.com/microsoft/vcpkg/issues/41143

azure-pipelines/end-to-end-tests-dir/cli.ps1
@@ -50,7 +50,7 @@ $out = Run-VcpkgAndCaptureOutput -TestArgs @('install', 'this-is-super-not-a-#po
 Throw-IfNotFailed
 
 [string]$expected = @"
-error: expected the end of input parsing a package spec; this usually means the indicated character is not allowed to be in a package spec. Port, triplet, and feature names are all lowercase alphanumeric+hypens.
+error: expected the end of input parsing a package spec; this usually means the indicated character is not allowed to be in a package spec. Port, triplet, and feature names are all lowercase alphanumeric+hyphens.
   on expression: this-is-super-not-a-#port
                                      ^
 
@@ -74,6 +74,21 @@ error: unknown binary provider type: valid providers are 'clear', 'default', 'nu
 if (-Not ($out.Replace("`r`n", "`n").EndsWith($expected)))
 {
     throw 'Bad malformed --binarysource output; it was: ' + $out
+}
+
+$out = Run-VcpkgAndCaptureOutput -TestArgs @('install', 'zlib', '--triplet', 'ARM-windows')
+Throw-IfNotFailed
+
+$expected = @"
+error: Invalid triplet name. Triplet names are all lowercase alphanumeric+hyphens.
+  on expression: ARM-windows
+                 ^
+Built-in Triplets:
+"@
+
+if (-Not ($out.Replace("`r`n", "`n").StartsWith($expected)))
+{
+    throw 'Bad malformed triplet output. It was: ' + $out
 }
 
 $out = Run-VcpkgAndCaptureStdErr -TestArgs @('x-package-info', 'zlib#notaport', '--x-json', '--x-installed')

include/vcpkg/base/message-data.inc.h
@@ -2207,25 +2207,25 @@ DECLARE_MESSAGE(
     (msg::package_name, msg::url),
     "",
     "\"{package_name}\" is not a valid feature name. "
-    "Feature names must be lowercase alphanumeric+hypens and not reserved (see {url} for more information).")
+    "Feature names must be lowercase alphanumeric+hyphens and not reserved (see {url} for more information).")
 DECLARE_MESSAGE(ParseIdentifierError,
                 (msg::value, msg::url),
                 "{value} is a lowercase identifier like 'boost'",
                 "\"{value}\" is not a valid identifier. "
-                "Identifiers must be lowercase alphanumeric+hypens and not reserved (see {url} for more information).")
+                "Identifiers must be lowercase alphanumeric+hyphens and not reserved (see {url} for more information).")
 DECLARE_MESSAGE(
     ParsePackageNameNotEof,
     (msg::url),
     "",
     "expected the end of input parsing a package name; this usually means the indicated character is not allowed to be "
-    "in a port name. Port names are all lowercase alphanumeric+hypens and not reserved (see {url} for more "
+    "in a port name. Port names are all lowercase alphanumeric+hyphens and not reserved (see {url} for more "
     "information).")
 DECLARE_MESSAGE(
     ParsePackageNameError,
     (msg::package_name, msg::url),
     "",
     "\"{package_name}\" is not a valid package name. "
-    "Package names must be lowercase alphanumeric+hypens and not reserved (see {url} for more information).")
+    "Package names must be lowercase alphanumeric+hyphens and not reserved (see {url} for more information).")
 DECLARE_MESSAGE(ParsePackagePatternError,
                 (msg::package_name, msg::url),
                 "",
@@ -2237,11 +2237,15 @@ DECLARE_MESSAGE(
     (),
     "",
     "expected the end of input parsing a package spec; this usually means the indicated character is not allowed to be "
-    "in a package spec. Port, triplet, and feature names are all lowercase alphanumeric+hypens.")
+    "in a package spec. Port, triplet, and feature names are all lowercase alphanumeric+hyphens.")
 DECLARE_MESSAGE(ParseQualifiedSpecifierNotEofSquareBracket,
                 (msg::version_spec),
                 "",
                 "expected the end of input parsing a package spec; did you mean {version_spec} instead?")
+DECLARE_MESSAGE(ParseTripletNotEof,
+                (),
+                "",
+                "Invalid triplet name. Triplet names are all lowercase alphanumeric+hyphens.")
 DECLARE_MESSAGE(PathMustBeAbsolute,
                 (msg::path),
                 "",

include/vcpkg/base/parse.h
@@ -21,6 +21,10 @@ namespace vcpkg
         int column;
     };
 
+    void append_caret_line(LocalizedString& res,
+                           const Unicode::Utf8Decoder& it,
+                           const Unicode::Utf8Decoder& start_of_line);
+
     struct ParseMessage
     {
         SourceLoc location = {};

src/vcpkg/base/parse.cpp
@@ -22,11 +22,13 @@ namespace vcpkg
         }
     }
 
-    static void append_caret_line(LocalizedString& res, const SourceLoc& loc)
+    void append_caret_line(LocalizedString& res,
+                           const Unicode::Utf8Decoder& cursor,
+                           const Unicode::Utf8Decoder& start_of_line)
     {
-        auto line_end = Util::find_if(loc.it, ParserBase::is_lineend);
+        auto line_end = Util::find_if(cursor, ParserBase::is_lineend);
         StringView line = StringView{
-            loc.start_of_line.pointer_to_current(),
+            start_of_line.pointer_to_current(),
             line_end.pointer_to_current(),
         };
 
@@ -42,7 +44,7 @@ namespace vcpkg
         std::string caret_string;
         caret_string.append(line_prefix_space, ' ');
         // note *it is excluded because it is where the ^ goes
-        for (auto it = loc.start_of_line; it != loc.it; ++it)
+        for (auto it = start_of_line; it != cursor; ++it)
         {
             if (*it == '\t')
                 caret_string.push_back('\t');
@@ -55,6 +57,11 @@ namespace vcpkg
         caret_string.push_back('^');
 
         res.append_indent().append_raw(caret_string);
+    }
+
+    static void append_caret_line(LocalizedString& res, const SourceLoc& loc)
+    {
+        append_caret_line(res, loc.it, loc.start_of_line);
     }
 
     LocalizedString ParseMessage::format(StringView origin, MessageKind kind) const

src/vcpkg/input.cpp
@@ -24,17 +24,22 @@ namespace vcpkg
 
     [[nodiscard]] ExpectedL<Unit> check_triplet(StringView name, const TripletDatabase& database)
     {
-        // Intentionally show the lowercased string
-        auto as_lower = Strings::ascii_to_lowercase(name);
-
-        if (std::find_if_not(name.begin(), name.end(), ParserBase::is_package_name_char) != name.end())
+        Unicode::Utf8Decoder start_of_line{name};
+        for (auto cursor = start_of_line; !cursor.is_eof(); ++cursor)
         {
-            return msg::format_error(msgParseQualifiedSpecifierNotEof);
+            if (!ParserBase::is_package_name_char(*cursor))
+            {
+                auto result = msg::format_error(msgParseTripletNotEof).append_raw('\n');
+                append_caret_line(result, cursor, start_of_line);
+                result.append_raw('\n');
+                append_help_topic_valid_triplet(result, database);
+                return result;
+            }
         }
 
-        if (!database.is_valid_triplet_canonical_name(as_lower))
+        if (!database.is_valid_triplet_canonical_name(name))
         {
-            LocalizedString result = msg::format_error(msgInvalidTriplet, msg::triplet = as_lower);
+            LocalizedString result = msg::format_error(msgInvalidTriplet, msg::triplet = name);
             result.append_raw('\n');
             append_help_topic_valid_triplet(result, database);
             return result;