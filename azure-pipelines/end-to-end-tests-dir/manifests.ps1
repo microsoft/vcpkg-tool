471f4efa30dd211ebbb3e2ac6c435f9989204966
Billy O'Neal <bion@microsoft.com>
2024-11-08 11:39:16 -0800

    Add switch to force classic mode. (#1535)
    A customer wanted to run vcpkg in a place where they could not control that a `vcpkg.json` was in a directory above, and requested a way to force classic mode. One used to be able to do this with the `--no-manifest` feature flag, but that was removed some years ago.
    
    This adds a new switch, `--classic`, which effectively skips looking for a manifest.

--- azure-pipelines/end-to-end-tests-dir/manifests.ps1
+++ azure-pipelines/end-to-end-tests-dir/manifests.ps1
@@ -185,3 +185,17 @@ Throw-IfFailed
 Write-Trace "test manifest features: self-reference, features = [a], with overlay"
 Run-Vcpkg install @manifestDirArgs --x-feature=a "--overlay-ports=$manifestDir/manifest-test"
 Throw-IfFailed
+
+Write-Trace "test manifest install with specific package names fails"
+$output = Run-VcpkgAndCaptureOutput install @manifestDirArgs vcpkg-empty-port
+Throw-IfNotFailed
+Throw-IfNonContains -Expected 'error: In manifest mode, `vcpkg install` does not support individual package arguments.' -Actual $output
+
+Write-Trace "test manifest install with specific package names forced to classic mode succeeds"
+$output = Run-VcpkgAndCaptureOutput install @manifestDirArgs --classic vcpkg-empty-port
+Throw-IfFailed
+$expected = @"
+The following packages will be built and installed:
+    vcpkg-empty-port:
+"@
+Throw-IfNonContains -Expected $expected -Actual $output
