9b3739efb3369c16d1e10f810744755aacaac880
Billy Robert O'Neal III <bion@microsoft.com>
2025-01-17 15:25:27 -0800

    Add missing ThrowIfFaileds

--- azure-pipelines/end-to-end-tests-dir/asset-caching.ps1
+++ azure-pipelines/end-to-end-tests-dir/asset-caching.ps1
@@ -3,7 +3,9 @@
 # Testing x-script
 Refresh-TestRoot
 Run-Vcpkg -TestArgs ($commonArgs + @('fetch', 'cmake'))
+Throw-IfFailed
 Run-Vcpkg -TestArgs ($commonArgs + @('fetch', 'ninja'))
+Throw-IfFailed
 Run-Vcpkg -TestArgs ($commonArgs + @("install", "vcpkg-test-x-script", "--x-binarysource=clear", "--overlay-ports=$PSScriptRoot/../e2e-ports", "--x-asset-sources=x-script,$TestScriptAssetCacheExe {url} {sha512} {dst};x-block-origin"))
 Throw-IfFailed
 
