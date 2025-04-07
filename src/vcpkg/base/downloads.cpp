21ca32378c48ba507f18f80215ba0c6da3b798c1
Tal Regev <tal.regev@gmail.com>
2025-04-07 14:26:16 +0300

    Add hacky way of code of upload by chunk

--- src/vcpkg/base/downloads.cpp
+++ src/vcpkg/base/downloads.cpp
@@ -823,6 +823,63 @@ namespace vcpkg
         return false;
     }
 
+    bool patch_file(const Filesystem& fs,
+                               StringView url,
+                               View<std::string> headers,
+                               const Path& file,
+                               std::size_t file_size,
+                               std::size_t chunk_size)
+    {
+        static constexpr StringLiteral guid_marker = "9a1db05f-a65d-419b-aa72-037fb4d0672e";
+
+        Command base_cmd;
+        base_cmd.string_arg("curl").string_arg("-X").string_arg("PATCH").string_arg("-w").string_arg(
+            "\\n" + guid_marker.to_string() + "%{http_code}\n");
+        for (auto&& header : headers)
+        {
+            base_cmd.string_arg("-H").string_arg(header);
+        }
+        base_cmd.string_arg(url);
+
+        auto file_ptr = fs.open_for_read(file, VCPKG_LINE_INFO);
+        std::vector<char> buffer(chunk_size);
+        std::size_t bytes_read = 0;
+        for (std::size_t i = 0; i < file_size; i += bytes_read)
+        {
+            bytes_read = file_ptr.read(buffer.data(), sizeof(decltype(buffer)::value_type), chunk_size);
+            if (!bytes_read)
+            {
+                msg::format_error(
+                    msgFileReadFailed, msg::path = file, msg::byte_offset = i, msg::count = chunk_size);
+                return false;
+            }
+
+            auto cmd = base_cmd;
+            cmd.string_arg("-H")
+                .string_arg(fmt::format("Content-Range: bytes {}-{}/{}", i, i + bytes_read - 1, file_size))
+                .string_arg("--data-binary")
+                .string_arg("@-");
+
+            int code = 0;
+            RedirectedProcessLaunchSettings launch_settings;
+            launch_settings.stdin_content = {buffer.data(), bytes_read};
+            auto res = cmd_execute_and_stream_lines(cmd, launch_settings, [&code](StringView line) {
+                if (Strings::starts_with(line, guid_marker))
+                {
+                    code = std::strtol(line.data() + guid_marker.size(), nullptr, 10);
+                }
+            });
+            if (!res.get() || *res.get() != 0 || (code >= 100 && code < 200) || code >= 300)
+            {
+                msg::format_error(
+                    msgCurlFailedToPutHttp, msg::exit_code = res.value_or(-1), msg::url = url, msg::value = code);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     bool store_to_asset_cache(DiagnosticContext& context,
                               StringView raw_url,
                               const SanitizedUrl& sanitized_url,
@@ -831,6 +888,9 @@ namespace vcpkg
                               const Path& file)
     {
         static constexpr StringLiteral guid_marker = "9a1db05f-a65d-419b-aa72-037fb4d0672e";
+        auto cache_size = real_filesystem.file_size(file, VCPKG_LINE_INFO);
+        return patch_file(real_filesystem, raw_url, headers, file, cache_size, 450*1024*1024);
+
 
         if (Strings::starts_with(raw_url, "ftp://"))
         {
