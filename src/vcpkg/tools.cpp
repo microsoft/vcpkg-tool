578c0da53cbfa5c5e51a5cb9979eb1fd2cd16205
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2025-09-09 16:12:38 +0200

    Everything inside */msys2/* is most likely the wrong cmake

--- src/vcpkg/tools.cpp
+++ src/vcpkg/tools.cpp
@@ -400,7 +400,7 @@ namespace vcpkg
             // the cmake version from mysys and cygwin can not be used because that version can't handle 'C:' in paths
             auto path = exe_path.generic_u8string();
             return !Strings::ends_with(path, "/usr/bin") && !Strings::ends_with(path, "/cygwin64/bin") &&
-                   !Strings::ends_with(path, "/msys64/ucrt64/bin");
+                   path.find("/msys2/") != std::string::npos;
         }
 
 #endif
