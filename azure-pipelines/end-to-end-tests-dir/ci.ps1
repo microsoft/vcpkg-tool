4ca3651e2cd9a593376014d4aaa993d1adea856c
Billy Robert O'Neal III <bion@microsoft.com>
2025-11-10 12:33:44 -0800

    Use Throw-IfNonContains and add assertions for counts

--- azure-pipelines/end-to-end-tests-dir/ci.ps1
+++ azure-pipelines/end-to-end-tests-dir/ci.ps1
@@ -5,42 +5,42 @@ $Output = Run-VcpkgAndCaptureOutput ci --dry-run --triplet=$Triplet --x-builtin-
 Throw-IfNotFailed
 $ErrorOutput = Run-VcpkgAndCaptureStdErr ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/ci"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.baseline.txt"
 Throw-IfNotFailed
-if (-not ($Output.Contains("dep-on-feature-not-sup:${Triplet}: cascade"))) {
-    throw 'dep-on-feature-not-sup must cascade because it depends on a features that is not supported'
-}
-if (-not ($Output.Contains("not-sup-host-b:${Triplet}: unsupported"))) {
-    throw 'not-sup-host-b must be skipped because it is not supported'
-}
-if (-not ($Output.Contains("feature-not-sup:${Triplet}:      *:"))) {
-    throw 'feature-not-sup must be built because the port that causes this port to skip should not be installed'
-}
-if (-not ($Output.Contains("feature-dep-missing:${Triplet}:      *:"))) {
-    throw 'feature-dep-missing must be built because the broken feature is not selected.'
-}
+# dep-on-feature-not-sup must cascade because it depends on a features that is not supported
+Throw-IfNonContains -Actual $Output -Expected "dep-on-feature-not-sup:${Triplet}: cascade"
+# not-sup-host-b must be skipped because it is not supported
+Throw-IfNonContains -Actual $Output -Expected "not-sup-host-b:${Triplet}: unsupported"
+# feature-not-sup must be built because the port that causes this port to skip should not be installed
+Throw-IfNonContains -Actual $Output -Expected "feature-not-sup:${Triplet}:      *:"
+# feature-dep-missing must be built because the broken feature is not selected.
+Throw-IfNonContains -Actual $Output -Expected "feature-dep-missing:${Triplet}:      *:"
 if ($Output.Split("*").Length -ne 4) {
     throw 'base-port should not be installed for the host'
 }
-if (-not ($ErrorOutput.Contains("REGRESSION: not-sup-host-b:${Triplet} is marked as fail but not supported for ${Triplet}."))) {
-    throw "feature-not-sup's baseline fail entry should result in a regression because the port is not supported"
-}
-if (-not ($ErrorOutput.Contains("REGRESSION: dep-on-feature-not-sup:${Triplet} is marked as fail but one dependency is not supported for ${Triplet}."))) {
-    throw "feature-not-sup's baseline fail entry should result in a regression because the port is cascade for this triplet"
-}
+Throw-IfNonContains -Actual $Output -Expected @"
+SUMMARY FOR $Triplet
+  CASCADED_DUE_TO_MISSING_DEPENDENCIES: 1
+  EXCLUDED_BY_DRY_RUN: 3
+  UNSUPPORTED: 1
+"@
+# feature-not-sup's baseline fail entry should result in a regression because the port is not supported
+Throw-IfNonContains -Actual $ErrorOutput -Expected "REGRESSION: not-sup-host-b:${Triplet} is marked as fail but not supported for ${Triplet}."
+# feature-not-sup's baseline fail entry should result in a regression because the port is cascade for this triplet
+Throw-IfNonContains -Actual $ErrorOutput -Expected "REGRESSION: dep-on-feature-not-sup:${Triplet} is marked as fail but one dependency is not supported for ${Triplet}."
 
 # pass means pass
-$Output = Run-VcpkgAndCaptureOutput ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/ci"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.baseline.txt"
+Run-Vcpkg ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/ci"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.baseline.txt"
 Throw-IfNotFailed
 $ErrorOutput = Run-VcpkgAndCaptureStdErr ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/ci"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.pass.baseline.txt"
 Throw-IfNotFailed
-if (-not ($ErrorOutput.Contains("REGRESSION: not-sup-host-b:${Triplet} is marked as pass but not supported for ${Triplet}."))) {
-    throw "feature-not-sup's baseline pass entry should result in a regression because the port is not supported"
-}
-if (-not ($ErrorOutput.Contains("REGRESSION: dep-on-feature-not-sup:${Triplet} cascaded, but it is required to pass. ("))) {
-    throw "feature-not-sup's baseline pass entry should result in a regression because the port is cascade for this triplet"
-}
+# feature-not-sup's baseline pass entry should result in a regression because the port is not supported
+Throw-IfNonContains -Actual $ErrorOutput -Expected "REGRESSION: not-sup-host-b:${Triplet} is marked as pass but not supported for ${Triplet}."
+# feature-not-sup's baseline pass entry should result in a regression because the port is cascade for this triplet
+Throw-IfNonContains -Actual $ErrorOutput -Expected "REGRESSION: dep-on-feature-not-sup:${Triplet} cascaded, but it is required to pass. ("
 
 # any invalid manifest must raise an error
-$Output = Run-VcpkgAndCaptureOutput ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/broken-manifests"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.baseline.txt"
+Remove-Problem-Matchers
+Run-Vcpkg ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/broken-manifests"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.baseline.txt"
+Restore-Problem-Matchers
 Throw-IfNotFailed
 
 # test malformed individual overlay port manifest
@@ -48,26 +48,24 @@ Remove-Problem-Matchers
 $Output = Run-VcpkgAndCaptureOutput ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/ci"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.baseline.txt" --overlay-ports="$PSScriptRoot/../e2e-ports/broken-manifests/malformed"
 Restore-Problem-Matchers
 Throw-IfNotFailed
-if (-not ($Output.Contains("vcpkg.json:3:17: error: Trailing comma"))) {
-    throw 'malformed port manifest must raise a parsing error'
-}
+# malformed port manifest must raise a parsing error
+Throw-IfNonContains -Actual $Output -Expected "vcpkg.json:3:17: error: Trailing comma"
 
 # test malformed overlay port manifests
 Remove-Problem-Matchers
 $Output = Run-VcpkgAndCaptureOutput ci --dry-run --triplet=$Triplet --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/ci"  --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci/ci.baseline.txt" --overlay-ports="$PSScriptRoot/../e2e-ports/broken-manifests"
 Restore-Problem-Matchers
 Throw-IfNotFailed
-if (-not ($Output.Contains("vcpkg.json:3:17: error: Trailing comma"))) {
-    throw 'malformed overlay port manifest must raise a parsing error'
-}
+# malformed overlay manifest must raise a parsing error
+Throw-IfNonContains -Actual $Output -Expected "vcpkg.json:3:17: error: Trailing comma"
 
 # test that file conflicts are detected
 Remove-Problem-Matchers
 $emptyDir = "$TestingRoot/empty"
 New-Item -ItemType Directory -Path $emptyDir -Force | Out-Null
 $Output = Run-VcpkgAndCaptureOutput ci @commonArgs --x-builtin-ports-root="$emptyDir" --binarysource=clear --overlay-ports="$PSScriptRoot/../e2e-ports/duplicate-file-a" --overlay-ports="$PSScriptRoot/../e2e-ports/duplicate-file-b"
-Throw-IfNotFailed
 Restore-Problem-Matchers
+Throw-IfNotFailed
 
 # test effect of parent hashes
 Remove-Item -Recurse -Force $installRoot -ErrorAction SilentlyContinue
@@ -84,18 +82,26 @@ Remove-Item -Recurse -Force $installRoot -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Path $installRoot -Force | Out-Null
 $Output = Run-VcpkgAndCaptureOutput ci @commonArgs --x-builtin-ports-root="$PSScriptRoot/../e2e-ports/ci" --binarysource="clear;files,$ArchiveRoot" --parent-hashes="$TestingRoot/parent-hashes.json"
 Throw-IfFailed
-if ($Output.Contains("base-port:${Triplet}: SUCCEEDED:")) {
-    throw 'base-port must not be rebuilt again'
-}
+# base-port must not be rebuilt again
+Throw-IfNonContains -Actual $Output -Expected "base-port:${Triplet}: parent: "
+Throw-IfNonContains -Actual $Output -Expected @"
+SUMMARY FOR $Triplet
+  CASCADED_DUE_TO_MISSING_DEPENDENCIES: 1
+  EXCLUDED_BY_PARENT: 3
+  UNSUPPORTED: 1
+"@
 
 # test that features included only by skipped ports are not included
-Remove-Problem-Matchers
 Refresh-TestRoot
+Remove-Problem-Matchers
 $Output = Run-VcpkgAndCaptureOutput ci @commonArgs --x-builtin-ports-root="$PSScriptRoot/../e2e-assets/ci-skipped-features" --binarysource=clear --ci-baseline="$PSScriptRoot/../e2e-assets/ci-skipped-features/baseline.txt"
+Restore-Problem-Matchers
 Throw-IfFailed
 if (-not ($Output -match 'skipped-features:[^:]+:      \*:' -and $Output -match 'Building skipped-features:[^@]+@1\.0\.0\.\.\.')) {
     throw 'did not attempt to build skipped-features'
 }
-
-Restore-Problem-Matchers
-
+Throw-IfNonContains -Actual $Output -Expected @"
+SUMMARY FOR $Triplet
+  SUCCEEDED: 1
+  EXCLUDED: 1
+"@
