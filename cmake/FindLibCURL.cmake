ecbe8dbd86a38e4eb3ae59b79030f75029b97c45
Billy Robert O'Neal III <bion@microsoft.com>
2026-01-28 13:40:35 -0800

    Go back to dlsym on Alpine, try updated headers to fix arm64-linux

--- cmake/FindLibCURL.cmake
+++ cmake/FindLibCURL.cmake
@@ -5,6 +5,7 @@ else()
 endif()
 
 option(VCPKG_LIBCURL_DLSYM "Select libcurl provider (SYSTEM|DLSYM)" "${VCPKG_LIBCURL_DLSYM_DEFAULT}")
+option(VCPKG_LIBCURL_DLSYM_UPDATED_HEADERS "Use more recent libcurl headers when using DLSYM than 7.29.0" OFF)
 
 if(POLICY CMP0135)
     cmake_policy(SET CMP0135 NEW)
@@ -15,15 +16,23 @@ if (NOT VCPKG_LIBCURL_DLSYM)
     return()
 endif()
 
-# This option exists to allow the URI to be replaced with a Microsoft-internal URI in official
+# The URI option exists to allow the URI to be replaced with a Microsoft-internal URI in official
 # builds which have restricted internet access; see azure-pipelines/signing.yml
 # Note that the SHA512 is the same, so vcpkg-tool contributors need not be concerned that we built
 # with different content.
-#
-# We're using curl-7_29_0 headers here because that's what comes with RHEL 7 (and inherited by
-# similarly ancient Oracle Linux 7)
-if(NOT VCPKG_LIBCURL_URL)
-    set(VCPKG_LIBCURL_URL "https://curl.se/download/archeology/curl-7.29.0.tar.gz")
+if (VCPKG_LIBCURL_DLSYM_UPDATED_HEADERS)
+    # curl-7_55_1 for https://daniel.haxx.se/blog/2017/06/15/target-independent-libcurl-headers/
+    # (mainly for arm64 support)
+    set(VCPKG_LIBCURL_HASH 4b0bf36a978b8b5ba66aecedbc2ae8ae9230da63ba5b80f9553d96671e013ccd679ee9cc10946c50b94d640858d74f3ec5d4e198c6b9f8842c941986d275cf7a)
+    if(NOT VCPKG_LIBCURL_URL)
+        set(VCPKG_LIBCURL_URL "https://curl.se/download/curl-7.55.1.tar.gz")
+    endif()
+else()
+    # curl-7_29_0 headers here because that's what comes with RHEL 7 (and inherited by similarly ancient Oracle Linux 7)
+    set(VCPKG_LIBCURL_HASH 08bafd09fa6d14362a426932fed8528c13133895477d8134c829e085637956d66d6be5a791057c1c04da04af6baa6496a6d59e00abf9ca6be5d29e798718b9bc)
+    if(NOT VCPKG_LIBCURL_URL)
+        set(VCPKG_LIBCURL_URL "https://curl.se/download/archeology/curl-7.29.0.tar.gz")
+    endif()
 endif()
 
 if(NOT TARGET CURL::libcurl)
@@ -31,7 +40,7 @@ include(FetchContent)
     FetchContent_Declare(
         LibCURLHeaders
         URL "${VCPKG_LIBCURL_URL}"
-        URL_HASH "SHA512=08bafd09fa6d14362a426932fed8528c13133895477d8134c829e085637956d66d6be5a791057c1c04da04af6baa6496a6d59e00abf9ca6be5d29e798718b9bc"
+        URL_HASH "SHA512=${VCPKG_LIBCURL_HASH}"
     )
 
     FetchContent_GetProperties(LibCURLHeaders)
