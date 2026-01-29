0cc0003f29f0a3d33b03346e47603e17314615b4
Billy Robert O'Neal III <bion@microsoft.com>
2026-01-28 16:45:43 -0800

    Fix some Linux build errors.

--- src/vcpkg/base/curl.cpp
+++ src/vcpkg/base/curl.cpp
@@ -54,7 +54,7 @@ void vcpkg_curl_global_init(long flags)
 
     if (!handle)
     {
-        vcpkg::Checks::msg_exit_with_error(vcpkg::msgUnableToFindCurl);
+        vcpkg::Checks::msg_exit_with_error(vcpkg::msg::msgUnableToFindCurl);
     }
 
     {
