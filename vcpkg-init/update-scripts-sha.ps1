f4731e62c67a5679e363adb1eb5356f42865077c
Billy O'Neal <bion@microsoft.com>
2026-02-12 16:46:04 -0800

    Update scripts sha 2026-02-12. (#1917)

--- vcpkg-init/update-scripts-sha.ps1
+++ vcpkg-init/update-scripts-sha.ps1
@@ -6,7 +6,12 @@ if (-not $sha) { throw "Failed to determine vcpkg scripts SHA." }
 
 "$sha`n" | Out-File -LiteralPath "$PSScriptRoot/vcpkg-scripts-sha.txt" -Encoding Ascii -NoNewline
 
-$configPath = (Resolve-Path -LiteralPath (Join-Path $PSScriptRoot '..\vcpkg-configuration.json')).Path
-$configText = Get-Content -LiteralPath $configPath -Raw -Encoding Ascii
-$newConfigText = $configText -replace '("baseline"\s*:\s*")[0-9a-f]+(")', "`$1$sha`$2"
-$newConfigText | Out-File -LiteralPath $configPath -Encoding Ascii -NoNewline
+$configPath = (Resolve-Path -LiteralPath (Join-Path $PSScriptRoot '../src/vcpkg-configuration.json')).Path
+$config = Get-Content -LiteralPath $configPath -Raw -Encoding Ascii | ConvertFrom-Json
+
+if (-not $config.'default-registry') { throw "Missing default-registry in vcpkg-configuration.json." }
+
+$config.'default-registry'.baseline = $sha
+$configJson = $config | ConvertTo-Json -Depth 100
+$configJson = $configJson -replace "`r`n", "`n"
+[System.IO.File]::WriteAllText($configPath, "$configJson`n", [System.Text.Encoding]::ASCII)
